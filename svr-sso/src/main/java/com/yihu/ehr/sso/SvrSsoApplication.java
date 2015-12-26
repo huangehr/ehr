fc1a4041cec82240e28806d3488897a048b9c0f7
Sand Wen <sand.fj.wen@gmail.com>
2015-12-26 17:10:46 +0800

    add sso service

---
+++ svr-sso/src/main/java/com/yihu/ehr/sso/SvrSsoApplication.java
@@ -0,0 +1,12 @@
+package com.yihu.ehr.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvrSsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SvrSsoApplication.class, args);
+    }
+}
