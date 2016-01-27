bb0a0189c75bdcc51837e18063ef3f484b00619e
cai <555@qq.com>
2016-01-27 17:57:53 +0800

    2016012702

--- ag-admin/src/main/java/com/yihu/ehr/ha/adapter/controller/OrgDictController.java
+++ ag-admin/src/main/java/com/yihu/ehr/ha/adapter/controller/OrgDictController.java
@@ -108,52 +108,52 @@ public class OrgDictController extends BaseRestController {
         return null;
     }
 
-    @RequestMapping(value="updateDictItem",produces = "text/html;charset=UTF-8")
+    @RequestMapping(value = "updateDictItem", produces = "text/html;charset=UTF-8")
     @ResponseBody
     public String updateDictItem(@ApiParam(name = "api_version", value = "API版本号", defaultValue = "v1.0")
-                                     @PathVariable(value = "api_version") String apiVersion,
-                                 @ApiParam(name = "id",value = "字典项ID")
-                                 @RequestParam(value = "id")String id,
+                                 @PathVariable(value = "api_version") String apiVersion,
+                                 @ApiParam(name = "id", value = "字典项ID")
+                                 @RequestParam(value = "id") String id,
                                  @ApiParam(name = "orgDictSeq", value = "字典序号")
-                                     @RequestParam(value = "orgDictSeq") int orgDictSeq,
+                                 @RequestParam(value = "orgDictSeq") int orgDictSeq,
                                  @ApiParam(name = "orgCode", value = "机构代码")
-                                     @RequestParam(value = "orgCode") String orgCode, @ApiParam(name = "code", value = "代码")
-                                     @RequestParam(value = "code") String code,
+                                 @RequestParam(value = "orgCode") String orgCode, @ApiParam(name = "code", value = "代码")
+                                 @RequestParam(value = "code") String code,
                                  @ApiParam(name = "name", value = "名称")
-                                     @RequestParam(value = "name") String name,
+                                 @RequestParam(value = "name") String name,
                                  @ApiParam(name = "description", value = "描述")
-                                     @RequestParam(value = "description") String description,
+                                 @RequestParam(value = "description") String description,
                                  @ApiParam(name = "sort", value = "排序")
-                                     @RequestParam(value = "sort") String sort,
+                                 @RequestParam(value = "sort") String sort,
                                  @ApiParam(name = "userId", value = "用户ID")
-                                     @RequestParam(value = "userId") String userId) {
+                                 @RequestParam(value = "userId") String userId) {
         return null;
     }
 
-        @RequestMapping(value = "/deleteOrgDictItem",method = RequestMethod.DELETE)
+    @RequestMapping(value = "/deleteOrgDictItem", method = RequestMethod.DELETE)
     @ApiOperation(value = "删除机构字典项", produces = "application/json", notes = "删除机构字典项信息，批量删除时，Id以逗号隔开")
     public String deleteOrgDictItem(@ApiParam(name = "api_version", value = "API版本号", defaultValue = "v1.0")
-                                        @PathVariable(value = "api_version") String apiVersion,
+                                    @PathVariable(value = "api_version") String apiVersion,
                                     @ApiParam(name = "ids", value = "字典ID")
-                                        @RequestParam(value = "ids") String ids) {
+                                    @RequestParam(value = "ids") String ids) {
         return null;
     }
 
     @RequestMapping(value = "/orgDictItems", method = RequestMethod.GET)
     public String getOrgDictItemsByCodeOrName(@ApiParam(name = "api_version", value = "API版本号", defaultValue = "v1.0")
-                                  @PathVariable(value = "api_version") String apiVersion,
-                                  @ApiParam(name = "orgCode", value = "机构代码")
-                                  @RequestParam(value = "orgCode") String orgCode,
-                                  @ApiParam(name = "orgDictSeq", value = "字典序号")
-                                  @RequestParam(value = "orgDictSeq") int orgDictSeq,
-                                  @ApiParam(name = "code", value = "代码")
-                                  @RequestParam(value = "code") String code,
-                                  @ApiParam(name = "name", value = "名称")
-                                  @RequestParam(value = "name") String name,
-                                  @ApiParam(name = "page", value = "当前页", defaultValue = "1")
-                                  @RequestParam(value = "page") int page,
-                                  @ApiParam(name = "rows", value = "每页行数", defaultValue = "20")
-                                  @RequestParam(value = "rows") int rows) {
+                                              @PathVariable(value = "api_version") String apiVersion,
+                                              @ApiParam(name = "orgCode", value = "机构代码")
+                                              @RequestParam(value = "orgCode") String orgCode,
+                                              @ApiParam(name = "orgDictSeq", value = "字典序号")
+                                              @RequestParam(value = "orgDictSeq") int orgDictSeq,
+                                              @ApiParam(name = "code", value = "代码")
+                                              @RequestParam(value = "code") String code,
+                                              @ApiParam(name = "name", value = "名称")
+                                              @RequestParam(value = "name") String name,
+                                              @ApiParam(name = "page", value = "当前页", defaultValue = "1")
+                                              @RequestParam(value = "page") int page,
+                                              @ApiParam(name = "rows", value = "每页行数", defaultValue = "20")
+                                              @RequestParam(value = "rows") int rows) {
         return null;
     }
 }
