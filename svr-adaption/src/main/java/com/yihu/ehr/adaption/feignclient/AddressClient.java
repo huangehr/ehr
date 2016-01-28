6c64fefb7d0d649028d715f027b18286024dd702
hh <hh@qq.com>
2016-01-28 16:09:30 +0800

    svr-adaption 、utils modify

---
+++ svr-adaption/src/main/java/com/yihu/ehr/adaption/feignclient/AddressClient.java
@@ -0,0 +1,45 @@
+package com.yihu.ehr.adaption.feignclient;
+
+import com.yihu.ehr.constrant.Result;
+import com.yihu.ehr.model.address.MAddress;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+
+/**
+ * Created by Administrator on 2016/1/4.
+ */
+@EnableFeignClients
+@FeignClient("svr-address")
+@RequestMapping("/rest/v1.0/address")
+public interface AddressClient {
+
+    @RequestMapping(value = "/saveAddress", method = RequestMethod.PUT)
+    @ApiOperation(value = "地址检查并保存")
+    public Result saveAddress(
+            @ApiParam(name = "country", value = "国家" , defaultValue = "")
+            @RequestParam(value = "country") String country,
+            @ApiParam(name = "province", value = "省" , defaultValue = "")
+            @RequestParam(value = "province") String province,
+            @ApiParam(name = "city", value = "市", defaultValue = "")
+            @RequestParam(value = "city") String city,
+            @ApiParam(name = "district", value = "县", defaultValue = "")
+            @RequestParam(value = "district") String district,
+            @ApiParam(name = "town", value = "", defaultValue = "")
+            @RequestParam(value = "town") String town,
+            @ApiParam(name = "street", value = "街道", defaultValue = "")
+            @RequestParam(value = "street") String street,
+            @ApiParam(name = "extra", value = "其他", defaultValue = "")
+            @RequestParam(value = "extra") String extra,
+            @ApiParam(name = "postalCode", value = "行政编码", defaultValue = "")
+            @RequestParam(value = "postalCode") String postalCode);
+
+}
