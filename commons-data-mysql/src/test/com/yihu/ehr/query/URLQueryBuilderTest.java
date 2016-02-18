8786ddfe294e3c664a307a301a69e751b2851add
Sand Wen <sand.fj.wen@gmail.com>
2016-02-18 09:34:35 +0800

    增加URLQueryBuilder单元测试

---
+++ commons-data-mysql/src/test/com/yihu/ehr/query/URLQueryBuilderTest.java
@@ -0,0 +1,32 @@
+package com.yihu.ehr.query;
+
+import org.apache.commons.lang.StringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Sand
+ * @version 1.0
+ * @created 2016.02.18 9:27
+ */
+public class URLQueryBuilderTest {
+
+    @Test
+    public void testToString() throws Exception {
+        URLQueryBuilder builder = new URLQueryBuilder();
+        builder.addField("name")
+                .addField("age")
+                .addField("address")
+                .addFilter("name", "?", "王", null)
+                .addFilter("age", ">=", "20", null)
+                .addSorter("name", true)
+                .addSorter("age", false)
+                .setPageNumber(20)
+                .setPageSize(10);
+
+        String url = builder.toString();
+        Assert.assertTrue("编码失败", StringUtils.isNotEmpty(url));
+    }
+}
