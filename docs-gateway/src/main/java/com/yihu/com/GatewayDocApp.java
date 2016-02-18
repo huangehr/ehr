3743f81fb772903941a6e56e522385c8b768019a
Sand Wen <sand.fj.wen@gmail.com>
2016-02-18 09:43:09 +0800

    重命名文档应用名称

--- docs-gateway/src/main/java/com/yihu/com/GatewayDocApp.java
+++ docs-gateway/src/main/java/com/yihu/com/GatewayDocApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocMsApiApplication {
+public class GatewayDocApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DocMsApiApplication.class, args);
+		SpringApplication.run(GatewayDocApp.class, args);
 	}
 }
--- docs-micro-service/src/main/java/com/yihu/com/MicroServiceDocApp.java
+++ docs-micro-service/src/main/java/com/yihu/com/MicroServiceDocApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocMsApiApplication {
+public class MicroServiceDocApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DocMsApiApplication.class, args);
+		SpringApplication.run(MicroServiceDocApp.class, args);
 	}
 }
