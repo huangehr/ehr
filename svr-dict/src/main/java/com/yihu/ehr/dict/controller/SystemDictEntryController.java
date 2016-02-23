7ca0dca675280a13e66ab347fe70fd5f4ba894d7
laz <laz@jkzl.com>
2016-02-23 17:55:39 +0800

    linaz

--- svr-dict/src/main/java/com/yihu/ehr/dict/controller/SystemDictEntryController.java
+++ svr-dict/src/main/java/com/yihu/ehr/dict/controller/SystemDictEntryController.java
@@ -66,17 +66,10 @@ public class SystemDictEntryController extends BaseRestController {
             @ApiParam(name = "entry", value = "字典JSON结构")
             @RequestParam(value = "entry") String entryJson) {
         SystemDictEntry entry = toEntity(entryJson, SystemDictEntry.class);
-
         SystemDict systemDict = dictService.retrieve(entry.getDictId());
         if (systemDict == null) throw new ApiException(ErrorCode.GetDictFaild, "所属字典不存在");
-
-        if (systemDictEntryService.isDictContainEntry(entry.getDictId(), entry.getCode())) {
-            throw new ApiException(ErrorCode.InvalidSysDictEntry, "字典项代码已存在");
-        }
         int nextSort = systemDictEntryService.getNextSN(entry.getDictId());
         entry.setSort(nextSort);
-//        SystemDictEntry systemDictEntry = new SystemDictEntry();
-       // entry.setSort(nextSort);
         systemDictEntryService.createDictEntry(entry);
 
         return convertToModel(entry, MDictionaryEntry.class, null);
@@ -101,14 +94,6 @@ public class SystemDictEntryController extends BaseRestController {
             @PathVariable(value = "dict_id") long dictId,
             @ApiParam(name = "code", value = "字典ID", defaultValue = "")
             @PathVariable(value = "code") String code) throws Exception{
-//        SystemDict systemDict = dictService.retrieve(dictId);
-//        if (systemDict == null) {
-//            return;
-//        }
-//
-//        if (!systemDictEntryService.isDictContainEntry(dictId, code)) {
-//            return;
-//        }
 
         systemDictEntryService.deleteDictEntry(dictId, code);
         return true;
@@ -128,5 +113,15 @@ public class SystemDictEntryController extends BaseRestController {
         systemDictEntryService.saveDictEntry(entry);
 
         return convertToModel(entry, MDictionaryEntry.class, null);
+    }
+
+    @RequestMapping(value = "/dictionaries/dict_id/code" , method = RequestMethod.GET)
+    @ApiOperation(value = "根基dictId和code判断提交的字典项名称是否已经存在")
+    boolean isAppNameExists(
+            @ApiParam(name = "dict_id", value = "dict_id", defaultValue = "")
+            @RequestParam(value = "dict_id") long dictId,
+            @ApiParam(name = "code", value = "code", defaultValue = "")
+            @RequestParam(value = "code") String code){
+        return systemDictEntryService.isDictContainEntry(dictId, code);
     }
 }
