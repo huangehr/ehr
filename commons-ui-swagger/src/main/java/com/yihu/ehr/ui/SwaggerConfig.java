b5bed9b5cc0ba06bddb5ac06a219f3e969ced842
Sand Wen <sand.fj.wen@gmail.com>
2016-02-29 08:55:48 +0800

    修复注解引用问题

--- commons-ui-swagger/src/main/java/com/yihu/ehr/ui/SwaggerConfig.java
+++ commons-ui-swagger/src/main/java/com/yihu/ehr/ui/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.yihu.ehr.ui;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.async.DeferredResult;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
