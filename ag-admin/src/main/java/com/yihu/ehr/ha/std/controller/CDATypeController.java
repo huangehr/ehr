7e9dfc7719b52b1be7aa1e09b6fc3b3e18629f4f
yww <yww@163.com>
2016-03-01 17:59:50 +0800

    标准来源，cda类别

--- ag-admin/src/main/java/com/yihu/ehr/ha/std/controller/CDATypeController.java
+++ ag-admin/src/main/java/com/yihu/ehr/ha/std/controller/CDATypeController.java
@@ -1,6 +1,7 @@
 package com.yihu.ehr.ha.std.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yihu.ehr.agModel.standard.cdaType.CdaTypeDetailModel;
 import com.yihu.ehr.agModel.standard.cdaType.CdaTypeModel;
 import com.yihu.ehr.constants.ApiVersion;
 import com.yihu.ehr.ha.std.service.CDATypeClient;
