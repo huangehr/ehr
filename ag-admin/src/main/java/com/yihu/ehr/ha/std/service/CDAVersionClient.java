ffd8c1315bac30e539cabde2824fdfc193e49bdd
yww <yww@163.com>
2016-03-01 11:58:13 +0800

    cdaVersion的controller、client、test文件修改

--- ag-admin/src/main/java/com/yihu/ehr/ha/std/service/CDAVersionClient.java
+++ ag-admin/src/main/java/com/yihu/ehr/ha/std/service/CDAVersionClient.java
@@ -25,7 +25,7 @@ public interface CDAVersionClient {
 
     @RequestMapping(value = "/cdaVersions", method = RequestMethod.GET)
     @ApiOperation(value = "适配采集标准")
-    Collection<MCDAVersion> searchCDAVersions(
+    Collection searchCDAVersions(
             @ApiParam(name = "fields", value = "返回的字段，为空返回全部字段", defaultValue = "id,name,secret,url,createTime")
             @RequestParam(value = "fields", required = false) String fields,
             @ApiParam(name = "filters", value = "过滤器，为空检索所有条件", defaultValue = "")
@@ -39,7 +39,7 @@ public interface CDAVersionClient {
 
 
     @RequestMapping(value = "/cdaVersions/{version}/isLatest", method = RequestMethod.GET)
-    @ApiOperation(value = "判断是否最新版本")
+    @ApiOperation(value = "判断是否是最新已发布版本")
     boolean isLatestVersion(
             @ApiParam(name = "version", value = "版本号", defaultValue = "")
             @PathVariable(value = "version") String version);
@@ -52,7 +52,7 @@ public interface CDAVersionClient {
 
 
     @RequestMapping(value = "/cdaVersion/{version}/drop", method = RequestMethod.DELETE)
-    @ApiOperation(value = "丢弃版本")
+    @ApiOperation(value = "删除版本版本（编辑状态/非编辑状态）")
     boolean dropCDAVersion(
             @ApiParam(name = "version", value = "版本号", defaultValue = "")
             @PathVariable(value = "version") String version);
@@ -71,19 +71,25 @@ public interface CDAVersionClient {
             @PathVariable(value = "version") String version);
 
     @RequestMapping(value = "/cdaVersion/{version}/rollbackToStage", method = RequestMethod.PUT)
-    @ApiOperation(value = "将最新的已发布版本回滚为编辑状态")
-    public boolean rollbackToStage(
+    @ApiOperation(value = "将最新的已发布版本修改为编辑状态")
+    boolean rollbackToStage(
             @ApiParam(name = "version", value = "版本号", defaultValue = "")
             @RequestParam(value = "version") String version);
 
 
-    @RequestMapping(value = "/cdaVersion/{strVersion}", method = RequestMethod.PUT)
-    @ApiOperation(value = "将最新的已发布版本回滚为编辑状态")
+    @RequestMapping(value = "/cdaVersion/{version}", method = RequestMethod.PUT)
+    @ApiOperation(value = "修改版本信息")
     public boolean updateVersion(
-            @ApiParam(name = "vesion", value = "版本号", defaultValue = "")
-            @RequestParam(value = "vesion") String vesion,
-            @ApiParam(name = "vesionName", value = "版本名称", defaultValue = "")
-            @RequestParam(value = "vesionName") String vesionName);
+            @ApiParam(name = "version", value = "版本号", defaultValue = "")
+            @PathVariable(value = "version") String version,
+            @ApiParam(name = "versionName", value = "版本名称", defaultValue = "")
+            @RequestParam(value = "versionName") String versionName,
+            @ApiParam(name = "userCode", value = "操作者", defaultValue = "")
+            @RequestParam(value = "userCode") String userCode,
+            @ApiParam(name = "inStage", value = "编辑状态", defaultValue = "")
+            @RequestParam(value = "inStage") int inStage,
+            @ApiParam(name = "baseVersion", value = "父版本", defaultValue = "")
+            @RequestParam(value = "baseVersion") String baseVersion);
 
 
     @RequestMapping(value = "/cdaVersion/checkName", method = RequestMethod.GET)
@@ -96,5 +102,11 @@ public interface CDAVersionClient {
     @RequestMapping(value = "/cdaVersion/existInStage", method = RequestMethod.GET)
     @ApiOperation(value = "检查是否存在处于编辑状态的版本")
     boolean existInStage();
+
+    @RequestMapping(value = "/cdaVersion/{version}", method = RequestMethod.GET)
+    @ApiOperation(value = "获取版本信息")
+    MCDAVersion getVersion(
+            @ApiParam(name = "version", value = "版本号", defaultValue = "")
+            @PathVariable(value = "version") String version);
 
 }
---
+++ ag-admin/src/test/java/com.yihu.ehr.ha/CDAVersionTests.java
@@ -0,0 +1,96 @@
+package com.yihu.ehr.ha;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yihu.ehr.ha.std.controller.CDAVersionController;
+import com.yihu.ehr.util.Envelop;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertNotEquals;
+
+
+/**
+ * Created by yww on 2016/2/29.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AgAdminApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class CDAVersionTests {
+    ApplicationContext applicationContext = new SpringApplicationBuilder()
+            .web(false).sources(AgAdminApplication.class).run();
+    ;
+
+    Envelop envelop = new Envelop();
+
+    @Autowired
+    CDAVersionController cdaVersionController;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void atestVersion() throws Exception {
+        // 分页查询------------------------------1 ok
+        String fields = "";
+        String filters = "";
+        String sorts = "";
+        int size = 15;
+        int page = 1;
+        envelop = cdaVersionController.searchCDAVersions(fields, filters, sorts, size, page);
+        assertNotEquals("没有匹配条件的cda版本！", envelop.isSuccessFlg(), false);
+
+        //检查是否存在编辑状态的cda版本-------------2ok
+        boolean flag7 = cdaVersionController.existInStage();
+        assertNotEquals("不存在编辑状态的cda版本！", flag7, false);
+
+        //判断是否是最新的已发布的版本 -----------------------3ok
+        boolean flag1 = cdaVersionController.isLatestVersion("56d3f3b33656");
+        //56d4f83b5dc2
+        assertNotEquals("该cda版本不是最新的！", flag1, false);
+
+        //判断版本名称是否已存在--------------------4
+        String versionName = "V4.1";
+        //V1.0
+        boolean flag8 = cdaVersionController.checkVersionName(versionName);
+        assertNotEquals("该版本名称不存在！", flag8, false);
+
+        //获取cda版本对象--------------------------5
+        envelop = cdaVersionController.getVersion("56395d75b854");
+        assertNotEquals("未查询到对应的cda版本！", envelop.isSuccessFlg(), false);
+
+    }
+
+    @Test
+    public void btestVersion() throws Exception {
+        //新增cda版本(只能有一个处于编辑状态的版本）------------------------------6ok
+        envelop = cdaVersionController.addVersion("sysyww");
+        assertNotEquals("新增cda版本失败！", envelop.isSuccessFlg(), false);
+
+        //更cda版本信息---------------------------7
+        envelop = cdaVersionController.updateVersion("", "V4.11", "sys001", 1, "568ce002559f");
+        assertNotEquals("更新cda版本失败！", envelop.isSuccessFlg(), false);
+
+        //cda新版本发布----------------------------6ok
+        boolean flag4 = cdaVersionController.commitVersion("56d4edb15dc0");
+        assertNotEquals("新cda版本发布失败！", flag4, false);
+
+        //将最新的已发布版本修改为编辑状态----------7
+        boolean flag5 = cdaVersionController.rollbackToStage("56d4edb15dc0");
+        assertNotEquals("已发布版本修改为编辑状态失败！", flag5, false);
+
+        //删除标准版本及对应的数据库表（没有编辑与非编辑状态限制），同时若存在子类版本，则将子类的baseVersion 设置为此删除版本的父级版本（继承版本的父级版本问题，微服务）------------------------------4
+//        boolean flag2 = cdaVersionController.dropCDAVersion("");
+//        assertNotEquals("删除cda版本失败！",flag2,false);
+//
+        //删除（编辑状态）标准版本-----------------------5
+//        boolean flag3 = cdaVersionController.revertVersion("");
+//        assertNotEquals("删除cda版本失败****！",flag3,false);
+    }
+}
