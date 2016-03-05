c8a53c96daffdddad3a38558e7a9bf68a6a030b8
laz <laz@jkzl.com>
2016-03-05 11:03:41 +0800

    linaz

---
+++ ag-ehr-platform/src/main/java/com/yihu/ehr/api/model/UserModel.java
@@ -0,0 +1,229 @@
+package com.yihu.ehr.api.model;
+
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class UserModel implements Serializable{
+
+    private String id;
+    private Date createDate;
+    private Date lastLoginTime;
+    private String email;
+    private String loginCode;
+    private String realName;
+    private String password;
+    private Boolean activated;
+    private String validateCode;
+    private String userType;
+    private String telephone;
+    private String martialStatus;
+    private String gender;
+    private String idCardNo;
+    private OrganizationModel organization;
+    // 未绑定属性
+    private String orgPost;
+    private String privilege;
+    private String role;
+
+    //特殊属性
+    private String major;
+    private String medicalRole;
+    private String techTitle;
+    private String adminTitle;
+
+    private String imgRemotePath;
+    private String imgLocalPath;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Date lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLoginCode() {
+        return loginCode;
+    }
+
+    public void setLoginCode(String loginCode) {
+        this.loginCode = loginCode;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getActivated() {
+        return activated;
+    }
+
+    public void setActivated(Boolean activated) {
+        this.activated = activated;
+    }
+
+    public String getValidateCode() {
+        return validateCode;
+    }
+
+    public void setValidateCode(String validateCode) {
+        this.validateCode = validateCode;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getMartialStatus() {
+        return martialStatus;
+    }
+
+    public void setMartialStatus(String martialStatus) {
+        this.martialStatus = martialStatus;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getIdCardNo() {
+        return idCardNo;
+    }
+
+    public void setIdCardNo(String idCardNo) {
+        this.idCardNo = idCardNo;
+    }
+
+    public OrganizationModel getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(OrganizationModel organization) {
+        this.organization = organization;
+    }
+
+    public String getOrgPost() {
+        return orgPost;
+    }
+
+    public void setOrgPost(String orgPost) {
+        this.orgPost = orgPost;
+    }
+
+    public String getPrivilege() {
+        return privilege;
+    }
+
+    public void setPrivilege(String privilege) {
+        this.privilege = privilege;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getMedicalRole() {
+        return medicalRole;
+    }
+
+    public void setMedicalRole(String medicalRole) {
+        this.medicalRole = medicalRole;
+    }
+
+    public String getTechTitle() {
+        return techTitle;
+    }
+
+    public void setTechTitle(String techTitle) {
+        this.techTitle = techTitle;
+    }
+
+    public String getAdminTitle() {
+        return adminTitle;
+    }
+
+    public void setAdminTitle(String adminTitle) {
+        this.adminTitle = adminTitle;
+    }
+
+    public String getImgRemotePath() {
+        return imgRemotePath;
+    }
+
+    public void setImgRemotePath(String imgRemotePath) {
+        this.imgRemotePath = imgRemotePath;
+    }
+
+    public String getImgLocalPath() {
+        return imgLocalPath;
+    }
+
+    public void setImgLocalPath(String imgLocalPath) {
+        this.imgLocalPath = imgLocalPath;
+    }
+}
---
+++ ag-ehr-platform/src/main/java/com/yihu/ehr/feign/OrganizationClient.java
@@ -0,0 +1,41 @@
+package com.yihu.ehr.feign;
+
+import com.yihu.ehr.api.model.OrganizationModel;
+import com.yihu.ehr.constants.ApiVersion;
+import com.yihu.ehr.constants.MicroServices;
+import com.yihu.ehr.model.org.MOrganization;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * @author Sand
+ * @version 1.0
+ * @created 2016.03.03 15:01
+ */
+@FeignClient(MicroServices.OrganizationMgr)
+@ApiIgnore
+public interface OrganizationClient {
+
+    @RequestMapping(value = ApiVersion.Version1_0 + "/organizations/{org_code}", method = GET)
+    OrganizationModel getOrg(@PathVariable(value = "org_code") String orgCode);
+
+    @RequestMapping(value = ApiVersion.Version1_0 + "/organizations/admin/{admin_login_code}", method = GET)
+    OrganizationModel getOrgByAdminLoginCode(@PathVariable(value = "admin_login_code") String adminLoginCode);
+
+    @RequestMapping(value = ApiVersion.Version1_0 + "/organizations", method = GET)
+    List<OrganizationModel> search(
+            @RequestParam(value = "fields") String fields,
+            @RequestParam(value = "filters") String filters,
+            @RequestParam(value = "sorts") String sorts,
+            @RequestParam(value = "page") int page,
+            @RequestParam(value = "size") int size);
+
+}
