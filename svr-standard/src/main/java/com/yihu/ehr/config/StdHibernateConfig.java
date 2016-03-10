d80f95165da83fead242f22337ee80e99347d134
laz <laz@jkzl.com>
2016-03-10 11:36:08 +0800

    linaz

--- svr-standard/src/main/java/com/yihu/ehr/config/StdHibernateConfig.java
+++ svr-standard/src/main/java/com/yihu/ehr/config/StdHibernateConfig.java
@@ -36,8 +36,11 @@ public class StdHibernateConfig extends HibernateConfig  {
 
     @Bean
     public StdSessionFactoryBean sessionFactory(BasicDataSource dataSource) throws Exception {
-        String clzPath = System.getProperty("user.home") + "/ehr/std/";
-        System.setProperty("java.class.path", clzPath);
+        String splitMark = System.getProperty("file.separator");
+        String path = System.getProperty("java.io.tmpdir") + splitMark + "ehr" + splitMark + "std" + splitMark;
+
+//        String clzPath = System.getProperty("user.home") + "/ehr/std/";
+        System.setProperty("java.class.path", path);
 
         List<Class> tableClass = createEntity(dataSource);
         StdSessionFactoryBean sessionFactory = new StdSessionFactoryBean();
