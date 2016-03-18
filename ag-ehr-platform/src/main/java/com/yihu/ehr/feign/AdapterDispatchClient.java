941a625e803a7cbd6c1ff82c6745eeabdd0110ec
hh <hh@qq.com>
2016-03-18 18:07:50 +0800

    标准下载 新接口

--- ag-ehr-platform/src/main/java/com/yihu/ehr/feign/AdapterDispatchClient.java
+++ ag-ehr-platform/src/main/java/com/yihu/ehr/feign/AdapterDispatchClient.java
@@ -4,8 +4,10 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.yihu.ehr.constants.ApiVersion;
 import com.yihu.ehr.constants.MicroServices;
 import com.yihu.ehr.util.RestEcho;
+import com.yihu.ehr.util.encrypt.RSA;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by AndyCai on 2016/3/3.
@@ -44,4 +49,15 @@ public interface AdapterDispatchClient {
             @RequestParam(value = "private_key", required = true) String privateKey,
             @RequestParam(value = "version_code", required = true) String versionCode,
             @PathVariable(value = "org_code") String orgcode);
+
+
+    @RequestMapping(value = "/adapter_dispatcher/{org_code}/source", method = RequestMethod.GET)
+    @ApiOperation(value = "下载适配数据包，此包内容包含：平台标准，机构标准与二者适配", response = RestEcho.class, produces = "application/json", notes = "获取采集标准及适配方案信息，文件以Base64编码，压缩格式为zip")
+    Map getAdaptionUrl(
+            @ApiParam(required = true, name = "private_key", value = "用户名")
+            @RequestParam(value = "private_key", required = true) String privateKey,
+            @ApiParam(required = true, name = "version_code", value = "适配标准版本")
+            @RequestParam(value = "version_code", required = true) String versionCode,
+            @ApiParam(required = true, name = "org_code", value = "机构代码")
+            @PathVariable(value = "org_code") String orgcode) ;
 }
