b15c618d022e55f6a35a0f886f666a7ceb2331c6
cai <555@qq.com>
2016-03-21 18:50:29 +0800

    2016032103 bug 修改

--- ag-admin/src/main/java/com/yihu/ehr/ha/std/service/DictClient.java
+++ ag-admin/src/main/java/com/yihu/ehr/ha/std/service/DictClient.java
@@ -22,7 +22,7 @@ import java.util.Map;
  * Created by wq on 2016/3/2.
  */
 
-@FeignClient(MicroServices.StandardMgr)
+@FeignClient(MicroServices.Standard)
 @RequestMapping(ApiVersion.Version1_0)
 @ApiIgnore
 public interface DictClient {
