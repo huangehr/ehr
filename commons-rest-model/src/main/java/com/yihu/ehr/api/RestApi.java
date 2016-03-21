484e3ecdba3a49fcc4f4a1ce3204494ea547fc51
cai <555@qq.com>
2016-03-21 18:35:16 +0800

    2016032102 网关Bug修改

--- commons-rest-model/src/main/java/com/yihu/ehr/api/RestApi.java
+++ commons-rest-model/src/main/java/com/yihu/ehr/api/RestApi.java
@@ -57,6 +57,8 @@ public class RestApi {
         public static final String ClientTokens             = "/securities/clients/{client_id}/tokens";
 
         public static final String Keys                     = "/securities/keys/{id}";
+        public static final String deleteOrgKey                ="/securities/org/keys/{org_code}";
+        public static final String deleteUserKey                ="/securities/user/keys/{user_id}";
         public static final String Tokens                   = "/securities/tokens/{id}";
     }
 
@@ -67,6 +69,7 @@ public class RestApi {
     public static class Standards{
         public static final String Sources                  = "/std/sources";
         public static final String Source                   = "/std/sources/{id}";
+        public static final String IsSourceCodeExist         ="/std/sources/code_is_exist";
 
         public static final String Types                    = "/std/types";
         public static final String Type                     = "/std/types/{id}";
