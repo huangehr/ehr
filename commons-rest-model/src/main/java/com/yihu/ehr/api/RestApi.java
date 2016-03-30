f06389d842fb4c4369dbd2f6ee3addaa82dbd4ca
laz <laz@jkzl.com>
2016-03-30 14:05:24 +0800

    linaz

--- commons-rest-model/src/main/java/com/yihu/ehr/api/RestApi.java
+++ commons-rest-model/src/main/java/com/yihu/ehr/api/RestApi.java
@@ -148,6 +148,7 @@ public class RestApi {
         public static final String UserVerification                 = "/users/verification";
         public static final String UserAdmin                        = "/users/admin/{user_id}";
         public static final String UserAdminPassword                = "/users/admin/{user_id}/password";
+        public static final String UserAdminPasswordReset          = "/users/admin/{user_id}/password_reset";
         public static final String UserAdminKey                     = "/users/admin/{user_id}/key";
         public static final String UserAdminContact                 = "/users/admin/{user_id}/contact";
         public static final String UserIdCardNoExistence            = "/user/id_card_no/existence";
