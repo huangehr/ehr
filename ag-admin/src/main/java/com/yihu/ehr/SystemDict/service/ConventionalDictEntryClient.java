3626e4062977c9f7b9e929f2ce4532d7013bd8bd
卓 <linzhuo@jkzl.com>
2016-05-11 10:07:35 +0800

    修改家庭关系无法正常显示问题

--- ag-admin/src/main/java/com/yihu/ehr/SystemDict/service/ConventionalDictEntryClient.java
+++ ag-admin/src/main/java/com/yihu/ehr/SystemDict/service/ConventionalDictEntryClient.java
@@ -136,6 +136,12 @@ public interface ConventionalDictEntryClient {
             @ApiParam(name = "code", value = "字典代码", defaultValue = "")
             @RequestParam(value = "code") String code);
 
+    @RequestMapping(value = "/dictionaries/family_relationship", method = RequestMethod.GET)
+    @ApiOperation(value = "获取家庭关系字典项", response = MConventionalDict.class, produces = "application/json")
+    MConventionalDict getFamilyRelationship(
+            @ApiParam(name = "code", value = "字典代码", defaultValue = "")
+            @RequestParam(value = "code") String code);
+
 
     @RequestMapping(value = "/dictionaries/std_source_types", method = RequestMethod.GET)
     @ApiOperation(value = "获取标准来源类型字典项", response = MConventionalDict.class, produces = "application/json")
