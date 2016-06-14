e86e0552a2ca4396f5c737bb5fd8b5c4b45f2ab0
laz <laz@jkzl.com>
2016-06-14 22:29:01 +0800

    机构根据id列表查询的问题，/MicroServices问题，资源模块pom问题

--- svr-resource/src/main/java/com/yihu/ehr/ResourceSvrApp.java
+++ svr-resource/src/main/java/com/yihu/ehr/ResourceSvrApp.java
@@ -2,19 +2,26 @@ package com.yihu.ehr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.jmx.support.RegistrationPolicy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSpringDataWebSupport
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-public class ResourceSvrApp {
+public class ResourceSvrApp extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(ResourceSvrApp.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ResourceSvrApp.class);
     }
 }
