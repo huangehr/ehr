b2af8f1c0a526107cd8042b7b35d789640139522
Sand Wen <sand.fj.wen@gmail.com>
2016-06-16 15:24:44 +0800

    去除security自动配置

--- ag-ehr-cloud/src/main/java/com/yihu/ehr/EHRPlatformGatewayApp.java
+++ ag-ehr-cloud/src/main/java/com/yihu/ehr/EHRPlatformGatewayApp.java
@@ -2,6 +2,8 @@ package com.yihu.ehr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
@@ -16,7 +18,7 @@ import java.net.SocketException;
 @EnableEurekaClient
 @EnableZuulProxy
 @EnableScheduling
-public class EHRPlatformGatewayApp  {
+public class EHRPlatformGatewayApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws SocketException {
 		SpringApplication.run(EHRPlatformGatewayApp.class, args);
