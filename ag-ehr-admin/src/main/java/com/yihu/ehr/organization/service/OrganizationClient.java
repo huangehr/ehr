934e1a038a43079da63682d187f632efd92e2710
卓 <linzhuo@jkzl.com>
2016-06-27 10:07:55 +0800

    解决省份查询问题·

--- ag-ehr-admin/src/main/java/com/yihu/ehr/organization/service/OrganizationClient.java
+++ ag-ehr-admin/src/main/java/com/yihu/ehr/organization/service/OrganizationClient.java
@@ -29,7 +29,7 @@ public interface OrganizationClient {
             @ApiParam(name = "fields", value = "返回的字段，为空返回全部字段", defaultValue = "")
             @RequestParam(value = "fields", required = false) String fields,
             @ApiParam(name = "filters", value = "过滤器，为空检索所有条件", defaultValue = "")
-            @RequestBody String filters,
+            @RequestBody(required = false) String filters,
             @ApiParam(name = "sorts", value = "排序，规则参见说明文档", defaultValue = "")
             @RequestParam(value = "sorts", required = false) String sorts,
             @ApiParam(name = "size", value = "分页大小", defaultValue = "15")
