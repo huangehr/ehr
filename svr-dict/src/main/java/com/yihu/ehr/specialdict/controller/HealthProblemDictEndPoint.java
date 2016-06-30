8aa33606731eac7facd6779c2fd9e411ee43327b
shine <2224988281@qq.com>
2016-06-30 11:11:31 +0800

    指标字典redis

--- svr-dict/src/main/java/com/yihu/ehr/specialdict/controller/HealthProblemDictEndPoint.java
+++ svr-dict/src/main/java/com/yihu/ehr/specialdict/controller/HealthProblemDictEndPoint.java
@@ -20,10 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping(ApiVersion.Version1_0)
