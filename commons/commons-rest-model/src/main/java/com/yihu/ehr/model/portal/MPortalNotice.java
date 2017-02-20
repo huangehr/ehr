7fef7c2f2eb05b23857cf46f199e2cfd62df494b
yeshijie <707081346@qq.com>
2017-02-20 10:47:20 +0800

    通知公告修改

---
+++ commons/commons-rest-model/src/main/java/com/yihu/ehr/model/portal/MPortalNotice.java
@@ -0,0 +1,74 @@
+package com.yihu.ehr.model.portal;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by yeshijie on 2017/2/17.
+ */
+public class MPortalNotice implements Serializable {
+
+    private Long id;
+    private Integer type;
+    private String title;
+    private String content;
+    private String fileId;
+    private String releaseAuthor;
+    private Date releaseDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public String getReleaseAuthor() {
+        return releaseAuthor;
+    }
+
+    public void setReleaseAuthor(String releaseAuthor) {
+        this.releaseAuthor = releaseAuthor;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
