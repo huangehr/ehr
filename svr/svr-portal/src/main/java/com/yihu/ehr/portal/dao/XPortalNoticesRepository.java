a08089735d904f1d84adb371370f968d733fac01
yeshijie <707081346@qq.com>
2017-02-20 10:49:47 +0800

    通知公告修改

---
+++ svr/svr-portal/src/main/java/com/yihu/ehr/portal/dao/XPortalNoticesRepository.java
@@ -0,0 +1,13 @@
+package com.yihu.ehr.portal.dao;
+
+import com.yihu.ehr.portal.service.PortalNotices;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ * 2017-02-04 add by ysj
+ */
+public interface XPortalNoticesRepository extends PagingAndSortingRepository<PortalNotices, Long> {
+
+
+
+}
