7db54e111d73d9b22f397a5e8f38867e84b1598b
hill9868 <hill9868@163.com>
2017-03-11 15:21:32 +0800

    微服务名变更

--- commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/MicroServices.java
+++ commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/MicroServices.java
@@ -9,26 +9,26 @@ package com.yihu.ehr.constants;
  */
 public class MicroServices {
     public static final String Adaption = "svr-standard";
-    public static final String Application = "svr-app-cws";
+    public static final String Application = "svr-app";
     public static final String Configuration = "svr-configuration";
-    public static final String Dictionary = "svr-dict-cws";
+    public static final String Dictionary = "svr-dict";
     public static final String Discovery = "svr-discovery";
     public static final String ESB = "svr-esb";
-    public static final String Geography = "svr-geography-cws";
+    public static final String Geography = "svr-geography";
     public static final String HealthProfile = "svr-health-profile";
-    public static final String Organization = "svr-org-cws";
+    public static final String Organization = "svr-org";
     public static final String Package = "svr-pack-mgr";
     public static final String PackageResolve = "svr-pack-resolve";
-    public static final String Patient = "svr-patient-cws";
-    public static final String Portal = "svr-portal-cws";
-    public static final String Security = "svr-security-cws";
-    public static final String Standard = "svr-standard-cws";
-    public static final String User = "svr-user-cws";
-    public static final String Family = "svr-family-cws";
-    public static final String FileResource = "svr-file-resource-cws";
+    public static final String Patient = "svr-patient";
+    public static final String Portal = "svr-portal";
+    public static final String Security = "svr-security";
+    public static final String Standard = "svr-standard";
+    public static final String User = "svr-user";
+    public static final String Family = "svr-family";
+    public static final String FileResource = "svr-file-resource";
     public static final String Cipher = "svr-cipher";
     public static final String Resource = "svr-resource";
-    public static final String SpecialDict = "svr-standard-cws";
+    public static final String SpecialDict = "svr-standard";
     public static final String ArchiveSecurity = "svr-archive-security";
     public static final String Authentication = "svr-authentication";
 }
