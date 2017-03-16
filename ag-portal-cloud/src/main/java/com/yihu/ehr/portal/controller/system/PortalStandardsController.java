9700997cd503bb35bac22fc6339de392e7ed67c1
hzp <317574974@qq.com>
2017-03-16 13:54:06 +0800

    改用Result

--- ag-portal-cloud/src/main/java/com/yihu/ehr/portal/controller/system/PortalStandardsController.java
+++ ag-portal-cloud/src/main/java/com/yihu/ehr/portal/controller/system/PortalStandardsController.java
@@ -3,6 +3,9 @@ package com.yihu.ehr.portal.controller.system;
 import com.yihu.ehr.agModel.portal.PortalStandardModel;
 import com.yihu.ehr.constants.ApiVersion;
 import com.yihu.ehr.controller.BaseController;
+import com.yihu.ehr.model.common.ListResult;
+import com.yihu.ehr.model.common.ObjectResult;
+import com.yihu.ehr.model.common.Result;
 import com.yihu.ehr.model.portal.MPortalStandards;
 import com.yihu.ehr.portal.service.PortalStandardsClient;
 import com.yihu.ehr.util.datetime.DateTimeUtil;
@@ -35,7 +38,7 @@ public class PortalStandardsController extends BaseController{
 
     @RequestMapping(value = "/portalStandards", method = RequestMethod.GET)
     @ApiOperation(value = "获取标准规范管理列表", notes = "根据查询条件获取标准规范管理列表在前端表格展示")
-    public Envelop searchPortalStandards(
+    public Result searchPortalStandards(
             @ApiParam(name = "fields", value = "返回的字段，为空返回全部字段", defaultValue = "id")
             @RequestParam(value = "fields", required = false) String fields,
             @ApiParam(name = "filters", value = "过滤器，为空检索所有条件", defaultValue = "")
@@ -48,127 +51,97 @@ public class PortalStandardsController extends BaseController{
             @RequestParam(value = "page", required = false) int page) {
 
         ResponseEntity<List<MPortalStandards>> responseEntity = portalStandardsClient.searchPortalStandards(fields, filters, sorts, size, page);
-        List<MPortalStandards> mPortalStandardsList = responseEntity.getBody();
-        List<PortalStandardModel>  portalStandardModels = new ArrayList<>();
-        for (MPortalStandards mPortalStandards : mPortalStandardsList) {
-            PortalStandardModel portalStandardModel = convertToModel(mPortalStandards, PortalStandardModel.class);
-            portalStandardModel.setReleaseDate(mPortalStandards.getReleaseDate() == null?"": DateTimeUtil.simpleDateTimeFormat(mPortalStandards.getReleaseDate()));
 
-            portalStandardModels.add(portalStandardModel);
-        }
 
-        //获取总条数
-        int totalCount = getTotalCount(responseEntity);
+        ListResult re = new ListResult(page, size);
+        re.setTotalCount(getTotalCount(responseEntity));
+        re.setDetailModelList(responseEntity.getBody());
 
-        Envelop envelop = getResult(portalStandardModels, totalCount, page, size);
-        return envelop;
+        return re;
     }
 
 
     @RequestMapping(value = "portalStandards/admin/{portalStandard_id}", method = RequestMethod.GET)
     @ApiOperation(value = "获取标准规范管理信息", notes = "标准规范管理信息")
-    public Envelop getPortalStandard(
+    public Result getPortalStandard(
             @ApiParam(name = "portalStandard_id", value = "", defaultValue = "")
             @PathVariable(value = "portalStandard_id") Long portalStandardId) {
         try {
             MPortalStandards mPortalStandards = portalStandardsClient.getPortalStandard(portalStandardId);
             if (mPortalStandards == null) {
-                return failed("标准规范管理信息获取失败!");
+                return Result.error("标准规范管理信息获取失败!");
             }
 
-            PortalStandardModel detailModel = convertToModel(mPortalStandards, PortalStandardModel.class);
-            detailModel.setReleaseDate(mPortalStandards.getReleaseDate() == null?"": DateTimeUtil.simpleDateTimeFormat(mPortalStandards.getReleaseDate()));
-
-            return success(detailModel);
+            ObjectResult re= new ObjectResult(true,"标准规范管理信息获取成功!");
+            re.setData(mPortalStandards);
+            return re;
         }
         catch (Exception ex){
             ex.printStackTrace();
-            return failedSystem();
+            return Result.error(ex.getMessage());
         }
     }
 
     @RequestMapping(value = "/portalStandards/admin/{portalStandard_id}", method = RequestMethod.DELETE)
     @ApiOperation(value = "删除标准规范管理", notes = "根据标准规范管理id删除医生")
-    public Envelop deletePortalStandard(
+    public Result deletePortalStandard(
             @ApiParam(name = "portalStandard_id", value = "标准规范管理编号", defaultValue = "")
             @PathVariable(value = "portalStandard_id") String portalStandardId) {
         try {
             boolean result = portalStandardsClient.deletePortalStandard(portalStandardId);
             if (!result) {
-                return failed("删除失败!");
+                return Result.error("删除失败!");
             }
-            return success(null);
+            return Result.success("删除成功!");
         } catch (Exception ex) {
             ex.printStackTrace();
-            return failedSystem();
+            return Result.error(ex.getMessage());
         }
     }
 
 
     @RequestMapping(value = "/portalStandard", method = RequestMethod.POST)
     @ApiOperation(value = "创建标准规范管理", notes = "重新绑定标准规范管理信息")
-    public Envelop createPortalStandard(
+    public Result createPortalStandard(
             @ApiParam(name = "portalStandard_json_data", value = "", defaultValue = "")
             @RequestParam(value = "portalStandard_json_data") String portalStandardJsonData) {
         try {
-            PortalStandardModel detailModel = objectMapper.readValue(portalStandardJsonData, PortalStandardModel.class);
-
-            String errorMsg = null;
-
-            if (StringUtils.isNotEmpty(errorMsg)) {
-                return failed(errorMsg);
-            }
-            MPortalStandards mPortalStandard = convertToMPortalStandard(detailModel);
-            mPortalStandard = portalStandardsClient.createPortalStandard(objectMapper.writeValueAsString(mPortalStandard));
+            MPortalStandards mPortalStandard = portalStandardsClient.createPortalStandard(portalStandardJsonData);
             if (mPortalStandard == null) {
-                return failed("保存失败!");
+                return Result.error("保存失败!");
             }
 
-            detailModel = convertToModel(mPortalStandard, PortalStandardModel.class);
-            return success(detailModel);
+            ObjectResult re= new ObjectResult(true,"保存成功!");
+            re.setData(mPortalStandard);
+            return re;
         } catch (Exception ex) {
             ex.printStackTrace();
-            return failedSystem();
+            return Result.error(ex.getMessage());
         }
     }
 
 
     @RequestMapping(value = "/portalStandard", method = RequestMethod.PUT)
     @ApiOperation(value = "修改标准规范管理", notes = "重新绑定标准规范管理信息")
-    public Envelop updatePortalStandard(
+    public Result updatePortalStandard(
             @ApiParam(name = "portalStandard_json_data", value = "", defaultValue = "")
             @RequestParam(value = "portalStandard_json_data") String portalStandardJsonData) {
         try {
-            PortalStandardModel detailModel = toEntity(portalStandardJsonData, PortalStandardModel.class);
-            String errorMsg = null;
 
-            if (StringUtils.isNotEmpty(errorMsg)) {
-                return failed(errorMsg);
-            }
-            MPortalStandards mPortalStandard = convertToMPortalStandard(detailModel);
-            mPortalStandard = portalStandardsClient.updatePortalStandard(objectMapper.writeValueAsString(mPortalStandard));
+            MPortalStandards mPortalStandard = portalStandardsClient.updatePortalStandard(portalStandardJsonData);
             if(mPortalStandard==null){
-                return failed("保存失败!");
+                return Result.error("修改失败!");
             }
 
-            detailModel = convertToModel(mPortalStandard, PortalStandardModel.class);
-            return success(detailModel);
+            ObjectResult re= new ObjectResult(true,"修改成功!");
+            re.setData(mPortalStandard);
+            return re;
         }
         catch (Exception ex){
             ex.printStackTrace();
-            return failedSystem();
+            return Result.error(ex.getMessage());
         }
     }
 
 
-    public MPortalStandards convertToMPortalStandard(PortalStandardModel detailModel) throws ParseException {
-        if(detailModel==null)
-        {
-            return null;
-        }
-        MPortalStandards mPortalStandards = convertToModel(detailModel,MPortalStandards.class);
-        mPortalStandards.setReleaseDate(DateTimeUtil.simpleDateTimeParse(detailModel.getReleaseDate()));
-
-        return mPortalStandards;
-    }
 }
