e5c49a454f0ac5f68c9e7f260b3cb9ffd030100a
hill9868 <hill9868@163.com>
2017-05-02 17:27:47 +0800

    多余代码修复

--- ag-portal-cloud/src/main/java/com/yihu/ehr/portal/common/WebSpringMvcConfig.java
+++ ag-portal-cloud/src/main/java/com/yihu/ehr/portal/common/WebSpringMvcConfig.java
@@ -1,5 +1,5 @@
 package com.yihu.ehr.portal.common;
-import com.yihu.ehr.portal.common.GlobalHandlerExceptionResolver;
+import com.yihu.ehr.exception.GlobalHandlerExceptionResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
