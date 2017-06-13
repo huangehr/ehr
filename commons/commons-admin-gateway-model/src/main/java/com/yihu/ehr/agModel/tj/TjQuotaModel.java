70221c9e82f1b66724abef7cf4c67acf9b4965ee
wangxingwang <756584395@qq.com>
2017-06-13 13:52:57 +0800

    20170613 wangxingwang 在TjQuotaModel添加getter setter

--- commons/commons-admin-gateway-model/src/main/java/com/yihu/ehr/agModel/tj/TjQuotaModel.java
+++ commons/commons-admin-gateway-model/src/main/java/com/yihu/ehr/agModel/tj/TjQuotaModel.java
@@ -24,4 +24,148 @@ public class TjQuotaModel {
     private String remark;
     private TjQuotaDataSourceModel tjQuotaDataSourceModel;
     private TjQuotaDataSaveModel tjQuotaDataSaveModel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCron() {
+        return cron;
+    }
+
+    public void setCron(String cron) {
+        this.cron = cron;
+    }
+
+    public String getExecType() {
+        return execType;
+    }
+
+    public void setExecType(String execType) {
+        this.execType = execType;
+    }
+
+    public Date getExecTime() {
+        return execTime;
+    }
+
+    public void setExecTime(Date execTime) {
+        this.execTime = execTime;
+    }
+
+    public String getJobClazz() {
+        return jobClazz;
+    }
+
+    public void setJobClazz(String jobClazz) {
+        this.jobClazz = jobClazz;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUser() {
+        return createUser;
+    }
+
+    public void setCreateUser(String createUser) {
+        this.createUser = createUser;
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getUpdateUser() {
+        return updateUser;
+    }
+
+    public void setUpdateUser(String updateUser) {
+        this.updateUser = updateUser;
+    }
+
+    public String getUpdateUserName() {
+        return updateUserName;
+    }
+
+    public void setUpdateUserName(String updateUserName) {
+        this.updateUserName = updateUserName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getDataLevel() {
+        return dataLevel;
+    }
+
+    public void setDataLevel(Integer dataLevel) {
+        this.dataLevel = dataLevel;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public TjQuotaDataSourceModel getTjQuotaDataSourceModel() {
+        return tjQuotaDataSourceModel;
+    }
+
+    public void setTjQuotaDataSourceModel(TjQuotaDataSourceModel tjQuotaDataSourceModel) {
+        this.tjQuotaDataSourceModel = tjQuotaDataSourceModel;
+    }
+
+    public TjQuotaDataSaveModel getTjQuotaDataSaveModel() {
+        return tjQuotaDataSaveModel;
+    }
+
+    public void setTjQuotaDataSaveModel(TjQuotaDataSaveModel tjQuotaDataSaveModel) {
+        this.tjQuotaDataSaveModel = tjQuotaDataSaveModel;
+    }
 }
