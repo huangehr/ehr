41336aac2e1e02229bae359251846b44076c9c62
jkzlzhoujie <08zhoujie87@163.com>
2017-06-20 08:56:52 +0800

    指标执行

--- commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/ServiceApi.java
+++ commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/ServiceApi.java
@@ -828,6 +828,8 @@ public class ServiceApi {
         public static final String GetTjQuotaById = "tj/getTjQuotaById/{id}";
         public static final String TjQuotaExistsName = "/tj/tjQuotaExistsName/{name}";
         public static final String TjQuotaExistsCode = "/tj/tjQuotaExistsCode/{code}";
+
+        public static final String TjQuotaExecute = "/job/execuJob";
     }
 
 
