a93ba3624d61f5ec287e9b1359839dc3eb0c03a2
jkzlzhoujie <08zhoujie87@163.com>
2017-06-20 19:18:05 +0800

    日志查询接口

--- svr/svr-ehr-basic/src/main/java/com.yihu.ehr/logs/controller/LogsEndPoint.java
+++ svr/svr-ehr-basic/src/main/java/com.yihu.ehr/logs/controller/LogsEndPoint.java
@@ -6,6 +6,7 @@ import com.yihu.ehr.constants.SessionAttributeKeys;
 import com.yihu.ehr.controller.BaseUIController;
 import com.yihu.ehr.controller.EnvelopRestEndPoint;
 import com.yihu.ehr.logs.model.WLYY_BUSINESS_LOG;
+import com.yihu.ehr.logs.model.WLYY_OPERATOR_LOG;
 import com.yihu.ehr.model.common.ListResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -35,11 +36,9 @@ public class LogsEndPoint extends EnvelopRestEndPoint {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    @RequestMapping(value = "/log/getLogs", method = RequestMethod.GET)
+    @RequestMapping(value = "/log/getBussinessLogs", method = RequestMethod.GET)
     @ApiOperation(value = "根据传入的参数进行MONGODB日志列表的查询", response = WLYY_BUSINESS_LOG.class, responseContainer = "List")
-    public ListResult getLogs(
-            @ApiParam(name = "logType", value = "日志类型", defaultValue = "")
-            @RequestParam(value = "logType", required = false) String logType,
+    public ListResult getBusinessLogs(
             @ApiParam(name = "data", value = "数据", defaultValue = "")
             @RequestParam(value = "data", required = false) String data,
             @ApiParam(name = "startDate", value = "查询开始时间", defaultValue = "")
@@ -68,10 +67,6 @@ public class LogsEndPoint extends EnvelopRestEndPoint {
             Criteria crCaller = new Criteria().where("caller").is(caller);
             criteriaList.add(crCaller);
         }
-        if(StringUtils.isNotEmpty(logType)){
-            Criteria crLogType = new Criteria().where("logType").is(logType);
-            criteriaList.add(crLogType);
-        }
         if(StringUtils.isNotEmpty(startDate)){
             Criteria crStarTime = new Criteria().where("time").gte(startDate);// 大于等于开始日期
             criteriaList.add(crStarTime);
@@ -90,6 +85,73 @@ public class LogsEndPoint extends EnvelopRestEndPoint {
         query.addCriteria(cr);
         query.limit(end - begin).skip(begin);//分页数据
         List<WLYY_BUSINESS_LOG> logsModelList =  mongoTemplate.find(query, WLYY_BUSINESS_LOG.class);
+        ListResult listResult = new ListResult();
+        if(logsModelList.size() > 0) {
+            listResult.setDetailModelList(logsModelList);
+            listResult.setSuccessFlg(true);
+            listResult.setTotalCount(logsModelList.size());
+            listResult.setCode(200);
+            listResult.setMessage("日志查询成功！");
+            listResult.setCurrPage(page);
+            listResult.setPageSize(size);
+        }else{
+            listResult.setCode(200);
+            listResult.setMessage("查询无数据");
+            listResult.setTotalCount(0);
+        }
+        return listResult;
+    }
+
+
+    @RequestMapping(value = "/log/getOperatorLogs", method = RequestMethod.GET)
+    @ApiOperation(value = "根据传入的参数进行MONGODB日志列表的查询", response = WLYY_OPERATOR_LOG.class, responseContainer = "List")
+    public ListResult getOperatorLogs(
+            @ApiParam(name = "data", value = "数据", defaultValue = "")
+            @RequestParam(value = "data", required = false) String data,
+            @ApiParam(name = "startDate", value = "查询开始时间", defaultValue = "")
+            @RequestParam(value = "startDate", required = false) String startDate,
+            @ApiParam(name = "endDate", value = "查询结束时间", defaultValue = "")
+            @RequestParam(value = "endDate", required = false) String endDate,
+            @ApiParam(name = "caller", value = "调用者", defaultValue = "")
+            @RequestParam(value = "caller", required = false) String caller,
+            @ApiParam(name = "sorts", value = "排序，规则参见说明文档", defaultValue = "")
+            @RequestParam(value = "sorts", required = false) String sorts,
+            @ApiParam(name = "size", value = "分页大小", defaultValue = "15")
+            @RequestParam(value = "size", required = false) int size,
+            @ApiParam(name = "page", value = "当前页码", defaultValue = "1")
+            @RequestParam(value = "page", required = false) int page) throws Exception{
+
+        int begin = (page - 1) * size + 1 ;
+        int end = page * size;
+        Query query = new Query();
+        Criteria cr = new Criteria();
+        List<Criteria> criteriaList = new ArrayList<>();
+        if(StringUtils.isNotEmpty(data)){
+            Criteria crData = new Criteria("data").regex(".*?" + data + ".*"); //模糊查询
+            criteriaList.add(crData);
+        }
+        if(StringUtils.isNotEmpty(caller)){
+            Criteria crCaller = new Criteria().where("caller").is(caller);
+            criteriaList.add(crCaller);
+        }
+        if(StringUtils.isNotEmpty(startDate)){
+            Criteria crStarTime = new Criteria().where("time").gte(startDate);// 大于等于开始日期
+            criteriaList.add(crStarTime);
+        }
+        if(StringUtils.isNotEmpty(endDate)){
+            Criteria  crEndTime = new Criteria().where("time").lte(endDate);// 小于结束日期 + 1
+            criteriaList.add(crEndTime);
+        }
+        if(criteriaList != null && criteriaList.size() > 0 ){
+            Criteria[] criterias = new  Criteria[criteriaList.size()];
+            for(int i=0 ;i < criteriaList.size() ; i++){
+                criterias[i] = criteriaList.get(i);
+            }
+            cr.andOperator(criterias);
+        }
+        query.addCriteria(cr);
+        query.limit(end - begin).skip(begin);//分页数据
+        List<WLYY_OPERATOR_LOG> logsModelList =  mongoTemplate.find(query, WLYY_OPERATOR_LOG.class);
         ListResult listResult = new ListResult();
         if(logsModelList.size() > 0) {
             listResult.setDetailModelList(logsModelList);
