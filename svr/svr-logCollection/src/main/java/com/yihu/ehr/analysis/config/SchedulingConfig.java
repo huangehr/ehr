cfda46790f38b562a07cdedbc3b4d1a278f8f18b
hill9868 <hill9868@163.com>
2017-06-24 11:30:10 +0800

    日志解析服务增加

--- svr/svr-logCollection/src/main/java/com/yihu/ehr/analysis/config/SchedulingConfig.java
+++ svr/svr-logCollection/src/main/java/com/yihu/ehr/analysis/config/SchedulingConfig.java
@@ -1,8 +1,5 @@
 package com.yihu.ehr.analysis.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
 /**
  * Created by Administrator on 2017/2/6.
  */
