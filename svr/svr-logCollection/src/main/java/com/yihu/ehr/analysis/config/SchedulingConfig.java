0069d1780baa33ddac7b1b9d58ff46f7e4b4bcaf
jkzlzhoujie <08zhoujie87@163.com>
2017-06-29 15:19:34 +0800

    日志解析工程 优化

--- svr/svr-logCollection/src/main/java/com/yihu/ehr/analysis/config/SchedulingConfig.java
+++ svr/svr-logCollection/src/main/java/com/yihu/ehr/analysis/config/SchedulingConfig.java
@@ -1,5 +1,8 @@
 package com.yihu.ehr.analysis.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * Created by Administrator on 2017/2/6.
  */
