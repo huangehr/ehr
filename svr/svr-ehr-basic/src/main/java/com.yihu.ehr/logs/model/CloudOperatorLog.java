7f7fc8349c7120d3321bc259c9a33271eaff19d5
jkzlzhoujie <08zhoujie87@163.com>
2017-07-20 14:12:19 +0800

    日志查询

--- svr/svr-ehr-basic/src/main/java/com.yihu.ehr/logs/model/CloudOperatorLog.java
+++ svr/svr-ehr-basic/src/main/java/com.yihu.ehr/logs/model/CloudOperatorLog.java
@@ -3,8 +3,8 @@ package com.yihu.ehr.logs.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "CloudPperatorLog")
-public class CloudPperatorLog {
+@Document(collection = "CloudOperatorLog")
+public class CloudOperatorLog {
     @Id
     private String id;              //uuid
     private String caller;         //业务操作者
