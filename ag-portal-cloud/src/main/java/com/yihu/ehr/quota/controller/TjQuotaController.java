389d550adb103853b0817f18b940845437342786
jkzlzhoujie <08zhoujie87@163.com>
2017-07-25 18:12:21 +0800

    指标告警接口

--- ag-portal-cloud/src/main/java/com/yihu/ehr/quota/controller/TjQuotaController.java
+++ ag-portal-cloud/src/main/java/com/yihu/ehr/quota/controller/TjQuotaController.java
@@ -3,13 +3,18 @@ package com.yihu.ehr.quota.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihu.ehr.agModel.tj.QuotaReportModel;
 import com.yihu.ehr.agModel.tj.ReultModel;
+import com.yihu.ehr.agModel.tj.TjQuotaModel;
 import com.yihu.ehr.constants.ApiVersion;
 import com.yihu.ehr.constants.ServiceApi;
 import com.yihu.ehr.controller.BaseController;
 import com.yihu.ehr.model.common.ObjectResult;
 import com.yihu.ehr.model.common.Result;
+import com.yihu.ehr.model.org.MOrganization;
 import com.yihu.ehr.model.tj.MTjQuotaModel;
 import com.yihu.ehr.model.tj.MTjQuotaWarn;
+import com.yihu.ehr.portal.service.function.GetInfoClient;
+import com.yihu.ehr.portal.service.function.OrganizationClient;
+import com.yihu.ehr.portal.service.function.UserClient;
 import com.yihu.ehr.quota.service.TjQuotaClient;
 import com.yihu.ehr.quota.service.TjQuotaJobClient;
 import com.yihu.ehr.util.rest.Envelop;
@@ -20,6 +25,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -36,6 +43,12 @@ public class TjQuotaController extends BaseController {
     @Autowired
     TjQuotaJobClient tjQuotaJobClient;
     @Autowired
+    UserClient userClient;
+    @Autowired
+    GetInfoClient getInfoClient;
+    @Autowired
+    OrganizationClient organizationClient;
+    @Autowired
     ObjectMapper objectMapper;
 
 
@@ -43,19 +56,38 @@ public class TjQuotaController extends BaseController {
     @ApiOperation(value = "获取指标预警信息", notes = "获取指标预警信息")
     public Result getTjQuotaWarn(@RequestParam(value = "userId") String userId){
         try {
+//            //获取用户所拥有的
+//            List<String> orgList = getInfoClient.getOrgCode(userId);
+//            //获取用户所拥有的区域
+//            List<String> districtList = getInfoClient.getDistrictByUserId (userId);
+            Map<String,String> param = new HashMap<>();
+            Collection<MOrganization> organizations = organizationClient.getOrgsByAddress("福建省","厦门市" ,null );
+            java.util.Iterator it = organizations.iterator();
+            String filters = "";
+            while(it.hasNext()){
+                MOrganization mOrganization = (MOrganization)it.next();
+                filters = filters + mOrganization.getOrgCode() + ",";
+            }
+            param.put("org",filters);
+            //后续过滤距离现在一个月
+
             List<MTjQuotaWarn> mTjQuotaWarns = tjQuotaClient.getTjQuotaWarn(userId);
             if (mTjQuotaWarns == null) {
                 return Result.error("获取失败!");
             }else {
                 for(MTjQuotaWarn mTjQuotaWarn : mTjQuotaWarns){
                     MTjQuotaModel mTjQuotaModel= tjQuotaClient.getByCode(mTjQuotaWarn.getQuotaCode());
-                    Envelop envelop = tjQuotaJobClient.getQuotaReport(Integer.valueOf(mTjQuotaModel.getId().toString()), null);
-
+                    Envelop envelop = tjQuotaJobClient.getQuotaReport(Integer.valueOf(mTjQuotaModel.getId().toString()),objectMapper.writeValueAsString(param));
                     QuotaReportModel quotaReportModels = objectMapper.convertValue(envelop.getObj(),QuotaReportModel.class);
-                    List<ReultModel> reultModels = quotaReportModels.getReultModelList();
-                    for(ReultModel reult :reultModels){
-                        mTjQuotaWarn.setQuotaCount(Integer.valueOf(reult.getValue()));
+                    List<ReultModel> reultModels = null;
+                    int quotaCount = 0;
+                    if(quotaReportModels!=null){
+                       reultModels = quotaReportModels.getReultModelList();
+                       for(ReultModel reult :reultModels){
+                           quotaCount = quotaCount +Integer.valueOf(reult.getValue());
+                       }
                     }
+                    mTjQuotaWarn.setQuotaCount(quotaCount);
                     if(Double.valueOf(mTjQuotaWarn.getValue()) < mTjQuotaWarn.getQuotaCount()){
                         mTjQuotaWarn.setStatus(1);
                     }else {
