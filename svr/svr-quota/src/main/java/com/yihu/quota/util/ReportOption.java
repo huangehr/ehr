953ed70de7586367a2fd09d415f1afc26524d734
jkzlzhoujie <08zhoujie87@163.com>
2017-08-10 11:31:10 +0800

    报表的 opeition 封装

--- svr/svr-quota/src/main/java/com/yihu/quota/util/ReportOption.java
+++ svr/svr-quota/src/main/java/com/yihu/quota/util/ReportOption.java
@@ -25,10 +25,17 @@ public class ReportOption {
 
     /**
      * 折线图
+     * @param title  标题
+     * @param legend 副标题
+     * @param lineName 折线1名称
+     * @param datalist 数据集 1
+     * @param lineCount  图表中折线数 小于等于2
+     * @param lineName 折线2名称
+     * @param data2list 当lineCount大于1时 Data2list 不为空
      */
-    public void getLineEchartOption(String title,String legend ,List<Map<String, Object>> Datalist) {
+    public Option getLineEchartOption(String title,String legend ,String lineName,List<Map<String, Object>> datalist,int lineCount ,String line2Name,List<Map<String, Object>> data2list) {
         Option option = new Option();
-        option.title("高度(km)与气温(°C)变化关系").tooltip(Trigger.axis).legend("高度(km)与气温(°C)");
+        option.title("高度与气温变化关系").tooltip(Trigger.axis).legend("高度(km)与气温(°C)");
 //        option.toolbox().show(true).feature(Tool.mark, Tool.dataView, new MagicType(Magic.line, Magic.bar), Tool.restore, Tool.saveAsImage);
 //        option.calculable(true);
 //        option.tooltip().trigger(Trigger.axis).formatter("Temperature : <br/>{b}km : {c}°C");
@@ -51,13 +58,28 @@ public class ReportOption {
         line.itemStyle().normal().lineStyle().shadowColor("rgba(0,0,0,0.4)");
         line.data(15, -50, -56.5, -46.5, -22.1, -2.5, -27.7, -55.7, -76.5);
 
+        Line line2 = new Line();
+        line2.data();
+        line2.smooth(true);
+        line2.name("高度(km)与气温(°C)变化关系");
+        line2.itemStyle().normal().lineStyle().shadowColor("rgba(0,0,0,0.4)");
+        if(data2list != null){
+            for (Map<String, Object> objectMap : data2list) {
+                line2.data(objectMap.get("TOTAL"));
+            }
+        }
         option.series(line);
+        return option;
     }
 
     /**
      * 柱状图
+     * @param title 标题
+     * @param legend 副标题
+     * @param datalist 数据集1
+     * @param data2list 数据集2
      */
-    public void getBarEchartOption(String title,String legend ,List<Map<String, Object>> Datalist) {
+    public Option getBarEchartOption(String title,String legend ,String barName,List<Map<String, Object>> datalist,String bar2Name ,List<Map<String, Object>> data2list) {
         List<Map<String, Object>> list = new ArrayList<>();
         //创建Option
         Option option = new Option();
@@ -75,19 +97,33 @@ public class ReportOption {
             //类目对应的柱状图
             bar.data(objectMap.get("TOTAL"));
         }
+        if(data2list != null){
+            Bar bar2 = new Bar("2 金额（元）");
+            for (Map<String, Object> objectMap : data2list) {
+                //设置类目
+                category.data(objectMap.get("NAME"));
+                //类目对应的柱状图
+                bar2.data(objectMap.get("TOTAL"));
+            }
+        }
         //设置类目轴
         option.yAxis(category);
         //设置数据
         option.series(bar);
         //由于药品名字过长，图表距离左侧距离设置180，关于grid可以看ECharts的官方文档
         option.grid().x(180);
+        return option;
     }
 
 
     /**
      * 饼状图
+     * @param title 标题
+     * @param legend 副标题
+     * @param datalist 数据集1
+     * @param data2list 数据集2
      */
-    public void getPieEchartOption(String title,String legend ,List<Map<String, Object>> Datalist) {
+    public Option getPieEchartOption(String title,String legend ,String pieName,List<Map<String, Object>> datalist ,String pie2Name,List<Map<String, Object>> data2list) {
         List<Map<String, Object>> list = new ArrayList<>();
         //创建Option
         Option option = new Option();
@@ -98,12 +134,22 @@ public class ReportOption {
         CategoryAxis category = new CategoryAxis();
         //饼图数据
         Pie pie = new Pie("金额（元）");
+
         //循环数据
         for (Map<String, Object> objectMap : list) {
             //设置类目
             category.data(objectMap.get("NAME"));
             //饼图数据
             pie.data(new PieData(objectMap.get("NAME").toString(), objectMap.get("TOTAL")));
+        }
+        if(data2list != null){
+            Pie pie2 = new Pie("金额（元）");
+            for (Map<String, Object> objectMap : data2list) {
+                //设置类目
+                category.data(objectMap.get("NAME"));
+                //饼图数据
+                pie2.data(new PieData(objectMap.get("NAME").toString(), objectMap.get("TOTAL")));
+            }
         }
         //设置类目轴
         option.yAxis(category);
@@ -113,13 +159,14 @@ public class ReportOption {
         option.series(pie);
         //由于药品名字过长，图表距离左侧距离设置180，关于grid可以看ECharts的官方文档
         option.grid().x(180);
+        return option;
     }
 
 
     /**
      * 柱状+饼状 图
      */
-    public void getBarPieEchartOption() {
+    public Option getBarPieEchartOption(String title,String legend ,List<Map<String, Object>> Datalist) {
         List<Map<String, Object>> list = new ArrayList<>();
         //创建Option
         Option option = new Option();
@@ -149,5 +196,6 @@ public class ReportOption {
         option.series(bar, pie);
         //由于药品名字过长，图表距离左侧距离设置180，关于grid可以看ECharts的官方文档
         option.grid().x(180);
+        return option;
     }
 }
