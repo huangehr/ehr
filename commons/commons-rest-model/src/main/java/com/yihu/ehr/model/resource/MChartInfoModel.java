3b9f90a341c348e6b20b1afdcf44fc89875d16fd
jkzlzhoujie <08zhoujie87@163.com>
2017-08-26 15:31:39 +0800

    指标图表预览 上卷 下钻

--- commons/commons-rest-model/src/main/java/com/yihu/ehr/model/resource/MChartInfoModel.java
+++ commons/commons-rest-model/src/main/java/com/yihu/ehr/model/resource/MChartInfoModel.java
@@ -1,12 +1,16 @@
 package com.yihu.ehr.model.resource;
 
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/8/12.
  */
 public class MChartInfoModel {
     private String title;
     private String option;
+    private List<Map<String,String>> listMap;
 
     public String getTitle() {
         return title;
@@ -22,5 +26,13 @@ public class MChartInfoModel {
 
     public void setOption(String option) {
         this.option = option;
+    }
+
+    public List<Map<String, String>> getListMap() {
+        return listMap;
+    }
+
+    public void setListMap(List<Map<String, String>> listMap) {
+        this.listMap = listMap;
     }
 }
