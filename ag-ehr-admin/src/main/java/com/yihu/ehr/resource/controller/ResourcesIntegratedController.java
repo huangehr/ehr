c4093c566bc0c90cd3799a2d2be7ea3f2f273dce
Progr1mmer <653800209@qq.com>
2017-08-29 19:44:58 +0800

    指标资源接口BUG修复

--- ag-ehr-admin/src/main/java/com/yihu/ehr/resource/controller/ResourcesIntegratedController.java
+++ ag-ehr-admin/src/main/java/com/yihu/ehr/resource/controller/ResourcesIntegratedController.java
@@ -3,6 +3,7 @@ package com.yihu.ehr.resource.controller;
 import com.yihu.ehr.constants.ApiVersion;
 import com.yihu.ehr.constants.ServiceApi;
 import com.yihu.ehr.controller.BaseController;
+import com.yihu.ehr.model.tj.MTjQuotaModel;
 import com.yihu.ehr.quota.service.TjQuotaClient;
 import com.yihu.ehr.quota.service.TjQuotaJobClient;
 import com.yihu.ehr.resource.client.ResourcesIntegratedClient;
@@ -115,8 +116,9 @@ public class ResourcesIntegratedController extends BaseController {
             @ApiParam(name="queryCondition", value="搜索条件",defaultValue = "")
             @RequestParam(value="queryCondition", required = false) String queryCondition) throws  Exception {
         Envelop envelop = new Envelop();
+        String [] quotaIdArr = quotaIds.split(",");
+        String [] quotaCodeArr = quotaCodes.split(",");
         try {
-            //拼接交集维度字符串作为查询参数
             String dimension = "";
             List<Map<String, String>> qsdList = tjQuotaClient.getTjQuotaSynthesiseDimension(quotaCodes);
             if(qsdList == null || qsdList.size() <= 0) {
@@ -127,10 +129,12 @@ public class ResourcesIntegratedController extends BaseController {
             for(Map<String, String> temp : qsdList) {
                 for(String codeStr : temp.keySet()){
                     if(quotaCodes.contains(codeStr)) {
+                        //添加键值对应列表
                         Map<String, String> objMap = new HashMap<String, String>();
                         objMap.put("key", temp.get(codeStr));
                         objMap.put("name", temp.get("name"));
                         objList.add(objMap);
+                        //结果总量参数
                         dimension += temp.get(codeStr) + ";";
                         break;
                     }
@@ -138,87 +142,118 @@ public class ResourcesIntegratedController extends BaseController {
             }
             //依次获取指标统计不同维度结果总量
             List<Envelop>  envelopList = new ArrayList<Envelop>();
-            String [] quotaIdArr = quotaIds.split(",");
             for(String id : quotaIdArr) {
+                //添加键值对应列表
+                MTjQuotaModel tjQuotaModel = tjQuotaClient.getById(new Long(id));
+                Map<String, String> objMap = new HashMap<String, String>();
+                objMap.put("key", tjQuotaModel.getCode());
+                objMap.put("name", tjQuotaModel.getName());
+                objList.add(objMap);
                 Envelop tempEnvelop = tjQuotaJobClient.getQuotaTotalCount(new Integer(id), queryCondition, dimension.substring(0, dimension.length() - 1));
                 envelopList.add(tempEnvelop);
             }
             List<Map<String, Object>> resultList = new ArrayList<Map<String, Object>>();
+            List<String> emptyList = new ArrayList<String>();
+            //遍历数据集，拼装结果集
             for(int i = 0; i < envelopList.size(); i ++ ) {
                 Envelop envelop1 = envelopList.get(i);
-                for(Map<String, Object> tempMap1 : (List<Map<String, Object>>)envelop1.getDetailModelList()) {
-                    Map<String, Object> newMap = new HashMap<String, Object>();
-                    //当记录为最后一个的时候
-                    if((envelopList.size() - 1) == i) {
-                        boolean isInResult = false;
-                        for(Map<String, Object> tempMap4 : resultList) {
-                            if(Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap4.get("cloumns")).toArray())) {
-                                isInResult = true;
-                            }
-                        }
-                        if(!isInResult) {
-                            newMap.put("cloumns", tempMap1.get("cloumns"));
-                            String resultStr = "";
-                            for(int x = 0; x < envelopList.size() - 1; x ++) {
-                                resultStr += "0,";
-                            }
-                            newMap.put("value", resultStr + tempMap1.get("value"));
-                        }
-                    }else {
-                        boolean isMatch = false;
-                        boolean isRecode = false;
-                        for (int j = i + 1; j < envelopList.size(); j++) {
-                            Envelop envelop2 = envelopList.get(j);
-                            for (Map<String, Object> tempMap2 : (List<Map<String, Object>>) envelop2.getDetailModelList()) {
-                                if (resultList.size() > 0) {
-                                    for (Map<String, Object> tempMap3 : resultList) {
-                                        if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap3.get("cloumns")).toArray())) {
-                                            isRecode = true;
-                                        }
-                                    }
-                                    if (!isRecode) {
-                                        if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap2.get("cloumns")).toArray())) {
-                                            newMap.put("cloumns", tempMap1.get("cloumns"));
-                                            String prefix = "";
-                                            for(int p = 0; p < i; p ++) {
-                                                prefix += "0," + prefix;
-                                            }
-                                            if (newMap.containsKey("value")) {
-                                                newMap.put("value", prefix + newMap.get("value") + "," + tempMap2.get("value"));
-                                            } else {
-                                                newMap.put("value", prefix + tempMap1.get("value") + "," + tempMap2.get("value"));
-                                            }
-                                            isMatch = true;
-                                        }
-                                    }
-                                } else {
-                                    if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap2.get("cloumns")).toArray())) {
-                                        newMap.put("cloumns", tempMap1.get("cloumns"));
-                                        if (newMap.containsKey("value")) {
-                                            newMap.put("value", newMap.get("value") + "," + tempMap2.get("value"));
-                                        } else {
-                                            newMap.put("value", tempMap1.get("value") + "," + tempMap2.get("value"));
-                                        }
-                                        isMatch = true;
-                                    }
+                if(envelop1.getDetailModelList().size() > 0) {
+                    //遍历当前数据集内的数据
+                    for (Map<String, Object> tempMap1 : (List<Map<String, Object>>) envelop1.getDetailModelList()) {
+                        Map<String, Object> newMap = new HashMap<String, Object>();
+                        //当记录为最后一个数据集的时候
+                        if ((envelopList.size() - 1) == i) {
+                            boolean isInResult = false;
+                            for (Map<String, Object> tempMap4 : resultList) {
+                                if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap4.get("cloumns")).toArray())) {
+                                    isInResult = true;
                                 }
                             }
-                            if (!isRecode && !isMatch) {
+                            if (!isInResult) {
                                 newMap.put("cloumns", tempMap1.get("cloumns"));
-                                String prefix = "";
-                                for(int p = 0; p < i; p ++) {
-                                    prefix += "0," + prefix;
-                                }
-                                if (newMap.containsKey("value")) {
-                                    newMap.put("value", prefix + newMap.get("value") + "," + "0");
-                                } else {
-                                    newMap.put("value", prefix + tempMap1.get("value") + "," + "0");
+                                for (int j = 0; j < quotaCodeArr.length; j++) {
+                                    if (i == j) {
+                                        newMap.put(quotaCodeArr[j], tempMap1.get("value"));
+                                    } else {
+                                        newMap.put(quotaCodeArr[j], 0);
+                                    }
                                 }
                             }
+                        } else {
+                            for (int j = i + 1; j < envelopList.size(); j++) {
+                                //是否匹配
+                                boolean isMatch = false;
+                                //时候已记录数据
+                                boolean isRecode = false;
+                                //与其他结果集进行对比
+                                Envelop envelop2 = envelopList.get(j);
+                                if(envelop2.getDetailModelList().size() > 0) {
+                                    for (Map<String, Object> tempMap2 : (List<Map<String, Object>>) envelop2.getDetailModelList()) {
+                                        if (resultList.size() > 0) {
+                                            for (Map<String, Object> tempMap3 : resultList) {
+                                                if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap3.get("cloumns")).toArray())) {
+                                                    isRecode = true;
+                                                }
+                                            }
+                                            if (!isRecode) {
+                                                if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap2.get("cloumns")).toArray())) {
+                                                    newMap.put("cloumns", tempMap1.get("cloumns"));
+                                                    newMap.put(quotaCodeArr[i], tempMap1.get("value"));
+                                                    newMap.put(quotaCodeArr[j], tempMap2.get("value"));
+                                                    for(int p = 0; p < i; p ++) {
+                                                        newMap.put(quotaCodeArr[p], 0);
+                                                    }
+                                                    isMatch = true;
+                                                }
+                                            }
+                                        } else {
+                                            if (Arrays.equals(((List<String>) tempMap1.get("cloumns")).toArray(), ((List<String>) tempMap2.get("cloumns")).toArray())) {
+                                                newMap.put("cloumns", tempMap1.get("cloumns"));
+                                                    newMap.put(quotaCodeArr[i], tempMap1.get("value"));
+                                                    newMap.put(quotaCodeArr[j], tempMap2.get("value"));
+                                                for(String tempCode : emptyList) {
+                                                    newMap.put(tempCode, 0);
+                                                }
+                                                isMatch = true;
+                                            }
+                                        }
+                                    }
+                                    if (!isRecode && !isMatch) {
+                                        newMap.put("cloumns", tempMap1.get("cloumns"));
+                                        newMap.put(quotaCodeArr[i], tempMap1.get("value"));
+                                        newMap.put(quotaCodeArr[j], 0);
+                                        for(int p = 0; p < i; p ++) {
+                                            newMap.put(quotaCodeArr[p], 0);
+                                        }
+                                    }
+                                }else {
+                                    if(!emptyList.contains(quotaCodeArr[j])) {
+                                        emptyList.add(quotaCodeArr[j]);
+                                    }
+                                    if(emptyList.size() > 0) {
+                                        for (Map<String, Object> tempMap : resultList) {
+                                            for (String tempCode : emptyList) {
+                                                tempMap.put(tempCode, 0);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                        if (newMap.size() > 0) {
+                            resultList.add(newMap);
                         }
                     }
-                    if(newMap.size() > 0) {
-                        resultList.add(newMap);
+                }else {
+                    if(!emptyList.contains(quotaCodeArr[i])) {
+                        emptyList.add(quotaCodeArr[i]);
+                    }
+                    if(emptyList.size() > 0) {
+                        for (Map<String, Object> tempMap : resultList) {
+                            for (String tempCode : emptyList) {
+                                tempMap.put(tempCode, 0);
+                            }
+                        }
                     }
                 }
             }
@@ -230,7 +265,11 @@ public class ResourcesIntegratedController extends BaseController {
                 for(int i = 0; i < colList.size(); i++) {
                     finalMap.put(dimensionArr[i], colList.get(i));
                 }
-                finalMap.put("value", tempMap.get("value"));
+                for(String key : tempMap.keySet()) {
+                    if(!key.equals("cloumns")) {
+                        finalMap.put(key, tempMap.get(key));
+                    }
+                }
                 finalList.add(finalMap);
             }
             envelop.setSuccessFlg(true);
@@ -275,6 +314,7 @@ public class ResourcesIntegratedController extends BaseController {
         }
         return envelop;
     }
+
 
 
 }
