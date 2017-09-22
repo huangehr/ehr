da72b540e90d7b27fb330f825dfcca1e3d557457
jkzlzhoujie <08zhoujie87@163.com>
2017-09-22 16:15:49 +0800

    指标数据权限

--- svr/svr-ehr-basic/src/main/java/com.yihu.ehr/geography/service/GeographyDictService.java
+++ svr/svr-ehr-basic/src/main/java/com.yihu.ehr/geography/service/GeographyDictService.java
@@ -35,7 +35,7 @@ public class GeographyDictService extends BaseJpaService<GeographyDict,XGeograph
 	}
 
     public GeographyDict findById(String id) {
-        return geographyDictRepository.findOne(Long.valueOf(id));
+        return geographyDictRepository.findOne(Integer.valueOf(id));
     }
 
     public GeographyDict findByName(String name) {
