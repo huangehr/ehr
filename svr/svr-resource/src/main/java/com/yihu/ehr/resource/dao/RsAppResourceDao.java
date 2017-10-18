4db914076b7962fbd0a133eab8d90d42e2a50589
wangxingwang <756584395@qq.com>
2017-10-18 10:36:09 +0800

    20171018 wxw 角色中初始化查询出的可授权视图列表，resourcesClient.queryResources()添加了两个参数

--- svr/svr-resource/src/main/java/com/yihu/ehr/resource/dao/RsAppResourceDao.java
+++ svr/svr-resource/src/main/java/com/yihu/ehr/resource/dao/RsAppResourceDao.java
@@ -1,11 +1,18 @@
 package com.yihu.ehr.resource.dao;
 
 import com.yihu.ehr.resource.model.RsAppResource;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by lyr on 2016/4/26.
  */
 public interface RsAppResourceDao extends PagingAndSortingRepository<RsAppResource, String> {
     RsAppResource findByAppId(String appId);
+
+    @Query("select ar.resourceId from RsAppResource ar where ar.appId = :appId")
+    List<String> findResourceIdListByAppId(@Param("appId") String appId);
 }
