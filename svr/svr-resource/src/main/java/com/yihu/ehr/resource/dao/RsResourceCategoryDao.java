ed60aa201446ab42a499e8218c703fb58fa00f40
Progr1mmer <653800209@qq.com>
2017-10-30 17:27:35 +0800

    授权方案-资源分类整改

--- svr/svr-resource/src/main/java/com/yihu/ehr/resource/dao/RsResourceCategoryDao.java
+++ svr/svr-resource/src/main/java/com/yihu/ehr/resource/dao/RsResourceCategoryDao.java
@@ -1,7 +1,9 @@
 package com.yihu.ehr.resource.dao;
 
 import com.yihu.ehr.resource.model.RsResourceCategory;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -12,4 +14,6 @@ public interface RsResourceCategoryDao extends PagingAndSortingRepository<RsReso
       long countByPid(String pid);
       List<RsResourceCategory> findByPid(String pid);
       List<RsResourceCategory> findByCode(String code);
+      @Query("SELECT rsResourceCategory FROM RsResourceCategory rsResourceCategory WHERE rsResourceCategory.code = :code AND rsResourceCategory.pid = :pid" )
+      List<RsResourceCategory> findByCodeAndPid(@Param("code") String code, @Param("pid") String pid);
 }
