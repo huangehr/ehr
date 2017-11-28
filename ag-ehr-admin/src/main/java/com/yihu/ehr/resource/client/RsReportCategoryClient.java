9321aaa167832843e5a69b6d982a3c24e14c39d4
jkzlzhoujie <08zhoujie87@163.com>
2017-11-28 15:02:11 +0800

    报表类型添加修改乱码

--- ag-ehr-admin/src/main/java/com/yihu/ehr/resource/client/RsReportCategoryClient.java
+++ ag-ehr-admin/src/main/java/com/yihu/ehr/resource/client/RsReportCategoryClient.java
@@ -45,13 +45,13 @@ public interface RsReportCategoryClient {
     @RequestMapping(value = ServiceApi.Resources.RsReportCategorySave, method = RequestMethod.POST)
     MRsReportCategory add(
             @ApiParam(name = "rsReportCategory", value = "资源报表分类JSON字符串", required = true)
-            @RequestParam(value = "rsReportCategory") String rsReportCategory);
+            @RequestBody String rsReportCategory);
 
     @ApiOperation("更新资源报表分类")
     @RequestMapping(value = ServiceApi.Resources.RsReportCategorySave, method = RequestMethod.PUT)
     MRsReportCategory update(
             @ApiParam(name = "rsReportCategory", value = "资源报表分类JSON字符串", required = true)
-            @RequestParam(value = "rsReportCategory") String rsReportCategory);
+            @RequestBody String rsReportCategory);
 
     @ApiOperation("删除资源报表分类")
     @RequestMapping(value = ServiceApi.Resources.RsReportCategoryDelete, method = RequestMethod.DELETE)
