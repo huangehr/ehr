f45fc2d19601b1e6becd7e1541450392759fca94
Progr1mmer <653800209@qq.com>
2017-12-10 20:40:40 +0800

    去掉获取档案解析消息为空的时候，关闭任务的代码，增加文件服务Swagger-UI下载文件到本地的功能

---
+++ svr/svr-pack-resolve/src/main/java/com/yihu/ehr/resolve/model/stage1/DataSetPackage.java
@@ -0,0 +1,63 @@
+package com.yihu.ehr.resolve.model.stage1;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.yihu.ehr.constants.ProfileType;
+import com.yihu.ehr.profile.annotation.Table;
+import com.yihu.ehr.profile.core.ResourceCore;
+import com.yihu.ehr.util.datetime.DateTimeUtil;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+/**
+ * 数据集档案包
+ *
+ * @author 张进军
+ * @created 2017.06.27 11:34
+ */
+@Table(ResourceCore.MasterTable)
+public class DataSetPackage extends StandardPackage {
+
+    private String pk;                              // 数据集主键（可能是联合主键）
+
+    private List<String> sqlList; // 遍历数据集拼接的插入/更新SQL语句
+
+    public DataSetPackage(){
+        setProfileType(ProfileType.DataSet);
+    }
+
+    public List<String> getSqlList() {
+        return sqlList;
+    }
+
+    public void setSqlList(List<String> sqlList) {
+        this.sqlList = sqlList;
+    }
+
+    public String getPk() {
+        return pk;
+    }
+
+    public void setPk(String pk) {
+        this.pk = pk;
+    }
+
+    @Override
+    public String toJson() {
+        ObjectNode root = objectMapper.createObjectNode();
+        root.put("cardId", this.getCardId());
+        root.put("orgCode", this.getOrgCode());
+        root.put("patientId", this.getPatientId());
+        root.put("eventNo", this.getEventNo());
+        root.put("cdaVersion", this.getCdaVersion());
+        root.put("clientId", this.getClientId());
+        root.put("eventTime", DateTimeUtil.utcDateTimeFormat(this.getEventDate()));
+        root.put("createTime", DateTimeUtil.utcDateTimeFormat(this.getCreateDate()));
+        root.put("eventType", StringUtils.isEmpty(this.getEventType()) ? "" : this.getEventType().toString());
+        root.put("profileType", this.getProfileType().toString());
+        root.put("cardType", this.getCardType());
+        root.put("patientName", this.getPatientName());
+        return root.toString();
+    }
+
+}
