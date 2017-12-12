5d73915bbf38df90cafef6d7b705c594206306ca
wangxingwang <756584395@qq.com>
2017-12-12 16:34:55 +0800

    修改

--- svr/svr-quota/src/main/java/com/yihu/quota/util/ReportOption.java
+++ svr/svr-quota/src/main/java/com/yihu/quota/util/ReportOption.java
@@ -388,7 +388,7 @@ public class ReportOption {
             pie2.name(pie2Name);
             pie2.type(SeriesType.pie);
             pie2.radius("40%");
-            pie2.center(new String[]{"50%", "50%"});
+            pie2.center(new String[]{"70%", "50%"});
             pie.itemStyle().normal().label().position("inner");
             pie2.itemStyle().emphasis().shadowBlur(10);
             pie2.itemStyle().emphasis().shadowOffsetX(0);
