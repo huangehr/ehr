4fc9db205850caeec8d90ec5cf853fd1becb2938
jkzlzhoujie <08zhoujie87@163.com>
2017-12-15 16:18:59 +0800

    Option 代码调整

--- svr/svr-quota/src/main/java/com/yihu/quota/util/ReportOption.java
+++ svr/svr-quota/src/main/java/com/yihu/quota/util/ReportOption.java
@@ -32,205 +32,6 @@ public class ReportOption {
     public static int radar = 4;
 
     /**
-     * 折线图
-     * @param title  标题
-     * @param legend 副标题
-     * @param xName x轴名称
-     * @param yName yz轴名称
-     * @param lineName 折线1名称
-     * @param datalist 数据集 1，key x 坐标轴数据列，val y轴坐标数据列
-     * @param lineName 折线2名称
-     * @param data2list 当lineCount大于1时 Data2list 不为空
-     */
-    public Option getLineEchartOption(String title, String legend ,String xName,String yName,String lineName, List<Map<String, Object>> datalist, String line2Name, List<Map<String, Object>> data2list) {
-        Option option = new GsonOption();
-        //title
-        option.title().setText(title);
-//        option.title().setSubtext(title);//小标题
-        option.title().x("center");
-        //tooltip
-        option.tooltip().trigger(Trigger.axis);
-        //toolbox
-//        option.toolbox().show(false);
-//        DataView dataView = new DataView();
-//        dataView.show(true);
-//        dataView.readOnly(false);
-//        option.toolbox().feature().put("dataView", Feature.dataView);
-//        MagicType magicType = new MagicType();
-//        magicType.show(true);
-//        List<String> typeList = new ArrayList<String>();
-//        typeList.add("pie");
-//        typeList.add("funnel");
-//        magicType.setType(typeList);
-//        option.toolbox().feature().put("magicType", magicType);
-//        Restore restore = new Restore();
-//        restore.show(true);
-//        option.toolbox().feature().put("restore", restore);
-//        SaveAsImage saveAsImage = new SaveAsImage();
-//        saveAsImage.show(true);
-//        option.toolbox().feature().put("saveAsImage", saveAsImage);
-
-        //legend (数据需填充)
-//        option.legend().orient(Orient.vertical);
-//        option.legend().left(X.left);
-//        List<String> legendDataList = new ArrayList<String>();
-//        legendDataList.add(legend);
-//        option.legend().data(legendDataList);
-        //grid
-        option.grid().x("3%");  // left
-        option.grid().y("22%");  // top
-        option.grid().x2("9%"); // right
-        option.grid().y2("3%"); // bottom
-//        option.grid().containLabel(true);
-        //yAxis
-        ValueAxis valueAxis = new ValueAxis();
-        valueAxis.name(yName);
-        valueAxis.type(AxisType.value);
-//        valueAxis.axisLabel().formatter("℃");//单位
-        option.yAxis(valueAxis);
-
-        //xAxis
-        CategoryAxis categoryAxis = new CategoryAxis();
-        categoryAxis.name(xName);
-        categoryAxis.type(AxisType.category);
-//        categoryAxis.axisLabel().formatter("千米");//单位
-//        categoryAxis.boundaryGap(false);
-
-        List<Object> lineNameList = getList(datalist,"NAME");
-        List<Object> lineValList = getList(datalist,"TOTAL");
-        Object[] lineName1 = (String[])lineNameList.toArray(new String[lineNameList.size()]);
-        categoryAxis.data(lineName1);
-        option.xAxis(categoryAxis);
-
-        //series
-        Line line = new Line();
-        line.name(lineName);
-        line.type(SeriesType.line);
-        line.smooth(true);
-        line.stack("stack");
-        line.itemStyle().normal().lineStyle().shadowColor("rgba(0,0,0,0.4)");
-
-        Object[] lineVal1 = (Object[])lineValList.toArray(new Object[lineValList.size()]);
-        line.data(lineVal1);
-        option.series().add(line);
-
-        if(data2list != null){
-            Line line2 = new Line();
-            line2.name(line2Name);
-            line2.type(SeriesType.line);
-            line2.smooth(true);
-            line2.stack("stack2");
-            line2.itemStyle().normal().lineStyle().shadowColor("rgba(0,0,0,0.4)");
-
-            List<Object> lineValList2 = getList(data2list,"TOTAL");
-            line2.data(lineValList2);
-            option.series().add(line2);
-        }
-        return option;
-    }
-
-    /**
-     * 柱状图
-     * @param title 标题
-     * @param legend 副标题
-     * @param datalist 数据集1
-     * @param data2list 数据集2
-     */
-    public Option getBarEchartOption(String title,String legend ,String xName,String yName ,String barName,List<Map<String, Object>> datalist, String bar2Name ,List<Map<String, Object>> data2list) {
-        Option option = new GsonOption();
-        //title
-        option.title().setText(title);
-//        option.title().setSubtext(title );
-        option.title().x("center");
-        //tooltip
-        option.tooltip().trigger(Trigger.axis);
-        option.tooltip().axisPointer().type(PointerType.shadow);
-        //toolbox
-//        option.toolbox().show(false);
-//        DataView dataView = new DataView();
-//        dataView.show(true);
-//        dataView.readOnly(false);
-//        option.toolbox().feature().put("dataView", Feature.dataView);
-//        MagicType magicType = new MagicType();
-//        magicType.show(true);
-//        List<String> typeList = new ArrayList<String>();
-//        typeList.add("pie");
-//        typeList.add("funnel");
-//        magicType.setType(typeList);
-//        option.toolbox().feature().put("magicType", magicType);
-//        Restore restore = new Restore();
-//        restore.show(true);
-//        option.toolbox().feature().put("restore", restore);
-//        SaveAsImage saveAsImage = new SaveAsImage();
-//        saveAsImage.show(true);
-//        option.toolbox().feature().put("saveAsImage", saveAsImage);
-
-        //legend (数据需填充)
-//        option.legend().orient(Orient.vertical);
-//        option.legend().left(X.left);
-//        List<String> legendDataList = new ArrayList<String>();
-//        legendDataList.add(legend);
-//        option.legend().data(legendDataList);
-        //grid
-        option.grid().x("3%");  // left
-        option.grid().y("22%");  // top
-        option.grid().x2("9%"); // right
-        option.grid().y2("3%"); // bottom
-//        option.grid().containLabel(true);
-        //yAxis
-        ValueAxis valueAxis = new ValueAxis();
-        valueAxis.name(yName);
-        valueAxis.type(AxisType.value);
-//        valueAxis.axisLabel().formatter("{value}℃");
-        option.yAxis(valueAxis);
-
-        //xAxis
-        CategoryAxis categoryAxis = new CategoryAxis();
-        categoryAxis.name(xName);
-        //categoryAxis.axisLine().onZero(false);
-        categoryAxis.type(AxisType.category);
-        //categoryAxis.axisLabel().formatter("千米");
-//        categoryAxis.boundaryGap(false);
-
-        List<Object> lineNameList = getList(datalist,"NAME");
-        List<Object> lineValList = getList(datalist,"TOTAL");
-        Object[] nameVal1 = (Object[])lineNameList.toArray(new Object[lineNameList.size()]);
-        categoryAxis.data(nameVal1);
-//        categoryAxis.splitLine().show(false);
-        option.xAxis(categoryAxis);
-
-        //series
-        Bar bar = new Bar();
-        bar.name(barName);
-        bar.type(SeriesType.bar);
-        bar.barWidth(20);
-        bar.clickable(true);
-        bar.itemStyle().normal().label().show(true);
-        bar.itemStyle().normal().label().position(Position.top);
-
-        Object[] lineVal1 = (Object[])lineValList.toArray(new Object[lineValList.size()]);
-        bar.data(lineVal1);
-        option.series().add(bar);
-
-        if(data2list != null){
-            Bar bar2 = new Bar();
-            bar2.name(bar2Name);
-            bar2.type(SeriesType.bar);
-            bar2.barWidth(20);
-            bar2.clickable(true);
-            bar2.itemStyle().normal().label().show(true);
-            bar2.itemStyle().normal().label().position(Position.top);
-
-            List<Object> lineValList2 = getList(data2list,"TOTAL");
-            Object[] lineVal2 = (Object[])lineValList2.toArray(new Object[lineValList2.size()]);
-            bar2.data(lineVal2);
-            option.series().add(bar2);
-        }
-
-        return option;
-    }
-
-    /**
      * 折线图和柱状  都支持
      * @param title  标题
      * @param xName x轴名称
@@ -249,10 +50,10 @@ public class ReportOption {
         option.tooltip().trigger(Trigger.axis);
         //toolbox
         //grid
-        option.grid().x("3%");  // left
-        option.grid().y("22%");  // top
-        option.grid().x2("9%"); // right
-        option.grid().y2("3%"); // bottom
+        option.grid().x("50px");  // left
+        option.grid().y("10px");  // top
+        option.grid().x2("50px"); // right
+        option.grid().y2("50px"); // bottom
 //        option.grid().containLabel(true);
         //yAxis
         ValueAxis valueAxis = new ValueAxis();
@@ -270,16 +71,7 @@ public class ReportOption {
 //        axisTick.setType
 //        categoryAxis.axisTick(axisTick);
 
-//        categoryAxis.axisLabel().formatter("千米");//单位
-
-        Object [] newData = new Object[5];
-        if(xData.length > 5 ){
-            newData = Arrays.copyOfRange(xData, 0, 5);
-            categoryAxis.data(newData);
-        }else{
-            categoryAxis.data(xData);
-        }
-
+        categoryAxis.data(xData);
         option.xAxis(categoryAxis);
 
 
@@ -300,16 +92,8 @@ public class ReportOption {
                 line.itemStyle().normal().lineStyle().shadowColor("rgba(0,0,0,0.4)");
 
                 List<Object> lineValList = lineDatas.get(i);
-
-                List<Object> subList = new ArrayList<>();
-                if(lineValList.size() > 5){
-                    subList = lineValList.subList(0, 5);
-                    Object[] lineVal = (Object[]) lineValList.toArray(new Object[subList.size()]);
-                    line.data(lineVal);
-                }else {
-                    Object[] lineVal = (Object[]) lineValList.toArray(new Object[lineValList.size()]);
-                    line.data(lineVal);
-                }
+                Object[] lineVal = (Object[]) lineValList.toArray(new Object[lineValList.size()]);
+                line.data(lineVal);
 
                 option.series().add(line);
             }
@@ -364,9 +148,9 @@ public class ReportOption {
 //        option.legend().data(legendDataList);
 
         //grid
-        option.grid().x("3%");  // left
-        option.grid().x2("4%"); // right
-        option.grid().y2("3%"); // bottom
+        option.grid().x("50px");  // left
+        option.grid().x2("10ox"); // right
+        option.grid().y2("30px"); // bottom
 //        option.grid().containLabel(true);
         //series
         Pie pie = new Pie();
