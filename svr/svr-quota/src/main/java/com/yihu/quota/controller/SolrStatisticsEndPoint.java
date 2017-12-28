bb7b37ae26f30876cb61ee67f9dd6586fe39c904
Progr1mmer <653800209@qq.com>
2017-12-28 20:35:45 +0800

    Solr统计

---
+++ svr/svr-quota/src/main/java/com/yihu/quota/controller/SolrStatisticsEndPoint.java
@@ -0,0 +1,132 @@
+package com.yihu.quota.controller;
+
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.netflix.eureka.V1AwareInstanceInfoConverter;
+import com.sun.tools.internal.xjc.model.nav.EagerNClass;
+import com.yihu.ehr.constants.ApiVersion;
+import com.yihu.ehr.constants.ServiceApi;
+import com.yihu.ehr.controller.EnvelopRestEndPoint;
+import com.yihu.ehr.query.common.model.QueryCondition;
+import com.yihu.ehr.query.services.SolrQuery;
+import com.yihu.ehr.solr.SolrUtil;
+import com.yihu.ehr.util.rest.Envelop;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by progr1mmer on 2017/12/28.
+ */
+@RestController
+@RequestMapping(value = ApiVersion.Version1_0)
+@Api(value = "SolrStatisticsEndPoint", description = "Solr统计", tags = {"数据统计-Solr统计"})
+public class SolrStatisticsEndPoint extends EnvelopRestEndPoint {
+
+    @Autowired
+    private SolrQuery solrQuery;
+    @Autowired
+    private SolrUtil solr;
+
+    @ApiOperation("每月就诊或住院人次")
+    @RequestMapping(value = "/statistics/mothVisit", method = RequestMethod.POST)
+    public Envelop statistics(
+            @ApiParam(name = "core", value = "集合", required = true)
+            @RequestParam(value = "core") String core,
+            @ApiParam(name = "filter", value = "查询条件", required = true)
+            @RequestParam(value = "filter") String filter) throws Exception {
+        Envelop envelop = new Envelop();
+        String queryParams = "";
+        List<QueryCondition> ql = parseCondition(filter);
+        queryParams = addParams(queryParams,"q", solrQuery.conditionToString(ql));
+        //Solr查询
+        Map<String, String> obj = objectMapper.readValue(queryParams, Map.class);
+        Map<String, Integer> dataMap = solr.getFacetQuery(core, obj.get("q"));
+        if(dataMap != null) {
+            envelop.setObj(dataMap.get(obj.get("q")));
+        }else {
+            envelop.setObj(0);
+        }
+        envelop.setSuccessFlg(true);
+        return envelop;
+        /**
+        SolrDocumentList solrList = solr.query(core, obj.get("q"), null, null, 0, 1000000);
+        List<String> list = new ArrayList<String>();
+        long count = 0;
+        if(solrList!=null && solrList.getNumFound()>0) {
+            count = solrList.getNumFound();
+            for (SolrDocument doc : solrList){
+                String rowkey = String.valueOf(doc.getFieldValue("rowkey"));
+                list.add(rowkey);
+            }
+        }
+        envelop.setSuccessFlg(true);
+        envelop.setObj(count);
+        return envelop;
+         */
+    }
+
+
+
+
+    /**
+     * 新增参数
+     * @return
+     */
+    private String addParams(String oldParams, String key,String value) {
+        String newParam = "";
+        if(value.startsWith("[") && value.endsWith("]")) {
+            newParam = "\""+key+"\":" + value;
+        }
+        else{
+            newParam = "\""+key+"\":\""+value.replace("\"","\\\"") + "\"";
+        }
+        if(oldParams!=null && oldParams.length()>3 && oldParams.startsWith("{") && oldParams.endsWith("}")) {
+            return oldParams.substring(0,oldParams.length()-1)+"," + newParam + "}";
+        }
+        else{
+            return "{"+newParam+"}";
+        }
+    }
+
+    /**
+     * 查询条件转换
+     * @param queryCondition
+     * @return
+     * @throws Exception
+     */
+    public List<QueryCondition> parseCondition(String queryCondition) throws IOException {
+        List<QueryCondition> ql = new ArrayList<QueryCondition>();
+        ObjectMapper mapper = new ObjectMapper();
+        JavaType javaType = mapper.getTypeFactory().constructParametricType(List.class, Map.class);
+        List<Map<String, Object>> list = objectMapper.readValue(queryCondition, javaType);
+        if(list != null && list.size() > 0) {
+            for(Map<String,Object> item : list) {
+                String andOr = String.valueOf(item.get("andOr")).trim();
+                String field = String.valueOf(item.get("field")).trim();
+                String cond = String.valueOf(item.get("condition")).trim();
+                String value = String.valueOf(item.get("value"));
+                if(value.indexOf(",")>0) {
+                    ql.add(new QueryCondition(andOr, cond, field, value.split(",")));
+                }
+                else{
+                    ql.add(new QueryCondition(andOr, cond, field, value));
+                }
+            }
+        }
+        return ql;
+    }
+
+}
