b74c5e3f7ede0cdf68f8b8e203145bc8350dd9e8
zdm <763558454@qq.com>
2017-12-28 21:51:12 +0800

    门诊仪表盘---by zdm

--- ag-ehr-admin/src/main/java/com/yihu/ehr/government/controller/OutpatientsBoardController.java
+++ ag-ehr-admin/src/main/java/com/yihu/ehr/government/controller/OutpatientsBoardController.java
@@ -4,7 +4,6 @@ import com.yihu.ehr.constants.ApiVersion;
 import com.yihu.ehr.constants.ServiceApi;
 import com.yihu.ehr.controller.BaseController;
 import com.yihu.ehr.government.service.OutpatientsBoardClient;
-import com.yihu.ehr.model.common.ListResult;
 import com.yihu.ehr.util.rest.Envelop;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -22,25 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Api(value = "outpatientsBoard", description = "政府服务平台-门诊仪表盘", tags = {"政府服务平台-门诊仪表盘"})
 public class OutpatientsBoardController extends BaseController {
+
         @Autowired
         private OutpatientsBoardClient outpatientsBoardClient;
 
         @RequestMapping(value = ServiceApi.Government.OutpatientsBoardCount, method = RequestMethod.POST)
         @ApiOperation(value = "根据统计周期类型、起止时间、指标编码统计结果")
         public Envelop outpatientsBoardCount(
-                @ApiParam(name = "periodType", value = " 周期类型：1-本月  2-本年", defaultValue = "")
-                @RequestParam(value = "periodType", required = false) String periodType,
-                @ApiParam(name = "startDate", value = " 周期开始时间", defaultValue = "")
-                @RequestParam(value = "startDate", required = false) String startDate,
-                @ApiParam(name = "endDate", value = " 周期结束时间", defaultValue = "")
-                @RequestParam(value = "endDate", required = false) String endDate,
-                @ApiParam(name = "quotaCode", value = " 指标编码", defaultValue = "")
-                @RequestParam(value = "quotaCode", required = false) String quotaCode) {
+                @ApiParam(name = "core", value = "集合", required = true)
+                @RequestParam(value = "core") String core,
+                @ApiParam(name = "filter", value = "查询条件", required = true)
+                @RequestParam(value = "filter") String filter) {
             Envelop envelop = new Envelop();
-
-            ListResult listResult=outpatientsBoardClient.outpatientsBoardCount(periodType,startDate,endDate,quotaCode);
-
-            envelop.setSuccessFlg(true);
+            envelop=outpatientsBoardClient.outpatientsBoardCount(core,filter);
             return envelop;
         }
 
