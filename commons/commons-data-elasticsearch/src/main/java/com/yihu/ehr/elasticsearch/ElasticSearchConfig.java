67b1df88c1f31ce54203d9331f0681e37e4967e3
Progr1mmer <653800209@qq.com>
2018-01-04 12:34:24 +0800

    dfs服务修改

--- commons/commons-data-elasticsearch/src/main/java/com/yihu/ehr/elasticsearch/ElasticSearchConfig.java
+++ commons/commons-data-elasticsearch/src/main/java/com/yihu/ehr/elasticsearch/ElasticSearchConfig.java
@@ -1,4 +1,4 @@
-package com.yihu.ehr.elasticsearch.config;
+package com.yihu.ehr.elasticsearch;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
--- commons/commons-data-elasticsearch/src/main/java/com/yihu/ehr/elasticsearch/ElasticSearchUtil.java
+++ commons/commons-data-elasticsearch/src/main/java/com/yihu/ehr/elasticsearch/ElasticSearchUtil.java
@@ -1,11 +1,12 @@
-package com.yihu.ehr.elasticsearch.service;
+package com.yihu.ehr.elasticsearch;
 
-import com.yihu.ehr.elasticsearch.dao.ElasticSearchDao;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.index.engine.DocumentMissingException;
 import org.elasticsearch.index.query.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -14,14 +15,15 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Service - Es搜索服务
+ * Util - Es搜索服务
  * Created by progr1mmer on 2017/12/2.
  */
 @Service
-public class ElasticSearchService {
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+public class ElasticSearchUtil {
 
     @Autowired
-    private ElasticSearchDao elasticSearchDao;
+    private ElasticSearchClient elasticSearchClient;
 
     public void mapping(String index, String type, Map<String, Map<String, String>> source) throws IOException{
         XContentBuilder xContentBuilder = XContentFactory.jsonBuilder().startObject().startObject("properties");
@@ -34,57 +36,57 @@ public class ElasticSearchService {
             xContentBuilder.endObject();
         }
         xContentBuilder.endObject().endObject();
-        elasticSearchDao.mapping(index, type, xContentBuilder);
+        elasticSearchClient.mapping(index, type, xContentBuilder);
     }
 
     public void remove(String index){
-        elasticSearchDao.remove(index);
+        elasticSearchClient.remove(index);
     }
 
     public Map<String, Object> index(String index, String type, Map<String, Object> source) throws ParseException{
-        return elasticSearchDao.index(index, type, source);
+        return elasticSearchClient.index(index, type, source);
     }
 
     public void delete(String index, String type, String [] idArr) {
-        elasticSearchDao.delete(index, type, idArr);
+        elasticSearchClient.delete(index, type, idArr);
     }
 
     public void deleteByField(String index, String type, String field, Object value) {
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchPhraseQuery(field, value);
         boolQueryBuilder.must(matchQueryBuilder);
-        List<String> idList = elasticSearchDao.getIds(index, type, boolQueryBuilder);
+        List<String> idList = elasticSearchClient.getIds(index, type, boolQueryBuilder);
         String [] idArr = new String[idList.size()];
         idArr = idList.toArray(idArr);
-        elasticSearchDao.delete(index, type, idArr);
+        elasticSearchClient.delete(index, type, idArr);
     }
 
     public Map<String, Object> update(String index, String type, String id, Map<String, Object> source) throws DocumentMissingException {
         if(source.containsKey("_id")) {
             source.remove("_id");
         }
-        return elasticSearchDao.update(index, type, id, source);
+        return elasticSearchClient.update(index, type, id, source);
     }
 
     public Map<String, Object> findById(String index, String type, String id) {
-        return elasticSearchDao.findById(index, type, id);
+        return elasticSearchClient.findById(index, type, id);
     }
 
     public List<Map<String, Object>> findByField(String index, String type, String field, Object value) {
         BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
         MatchQueryBuilder matchQueryBuilder = QueryBuilders.matchPhraseQuery(field, value);
         boolQueryBuilder.must(matchQueryBuilder);
-        return elasticSearchDao.findByField(index, type, boolQueryBuilder);
+        return elasticSearchClient.findByField(index, type, boolQueryBuilder);
     }
 
     public List<Map<String, Object>> page(String index, String type, List<Map<String, Object>> filter, int page, int size) {
         QueryBuilder boolQueryBuilder = getQueryBuilder(filter);
-        return elasticSearchDao.page(index, type, boolQueryBuilder, page, size);
+        return elasticSearchClient.page(index, type, boolQueryBuilder, page, size);
     }
 
     public long count(String index, String type, List<Map<String, Object>> filter) {
         QueryBuilder boolQueryBuilder = getQueryBuilder(filter);
-        return elasticSearchDao.count(index, type, boolQueryBuilder);
+        return elasticSearchClient.count(index, type, boolQueryBuilder);
     }
 
     private QueryBuilder getQueryBuilder(List<Map<String, Object>> filter) {
