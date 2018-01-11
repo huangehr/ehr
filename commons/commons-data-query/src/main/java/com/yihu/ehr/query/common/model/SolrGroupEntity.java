56824ae2f840d11e5afe764f865ee8367d40997c
zhangjinjun <613zjj>
2018-01-11 15:25:40 +0800

    整改solr抽取数据只按天+指标配置维度组合统计，生成每天的统计数据为最小统计单位。 故在solr抽取数据中，不做按周、月、年等时间维度统计。

--- commons/commons-data-query/src/main/java/com/yihu/ehr/query/common/model/SolrGroupEntity.java
+++ commons/commons-data-query/src/main/java/com/yihu/ehr/query/common/model/SolrGroupEntity.java
@@ -1,6 +1,7 @@
 package com.yihu.ehr.query.common.model;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -9,47 +10,57 @@ import java.util.Map;
  */
 public class SolrGroupEntity {
 
-    private String groupField; //分组字段名
-    private Map<String,String> groupCondition; //分组条件
-
-    /**
-     * 构造函数
-     */
-    public SolrGroupEntity(String groupField)
-    {
-        this.groupField = groupField;
-        this.groupCondition= new HashMap<String,String>();
+    // 分组统计类型
+    public static enum GroupType {
+        FIELD_VALUE, // 按字段值统计
+        DATE_RANGE, // 日期范围统计
+        NUMERIC_RANGE // 数值范围统计
     }
 
-    /**
-     * 构造函数
-     */
-    public SolrGroupEntity(String groupField, Map<String, String> groupCondition)
-    {
+    private String groupField; // 分组字段名
+    private GroupType type; // 分组统计类型
+    private Map<String, String> groupCondition = new LinkedHashMap<>(); // 分组条件
+
+    public SolrGroupEntity(String groupField) {
         this.groupField = groupField;
-        this.groupCondition= groupCondition;
+        this.type = GroupType.FIELD_VALUE;
+        this.groupCondition = new HashMap<String, String>();
     }
 
-    /**
-     * 获取分组字段名
-     * @return
-     */
+    public SolrGroupEntity(String groupField, Map<String, String> groupCondition) {
+        this.groupField = groupField;
+        this.type = GroupType.FIELD_VALUE;
+        this.groupCondition = groupCondition;
+    }
+
+    public SolrGroupEntity(String groupField, GroupType type, Map<String, String> groupCondition) {
+        this.groupField = groupField;
+        this.type = type;
+        this.groupCondition = groupCondition;
+    }
+
     public String getGroupField() {
         return groupField;
     }
 
-    /**
-     * 获取分组条件
-     * @return
-     */
+    public void setGroupField(String groupField) {
+        this.groupField = groupField;
+    }
+
+    public GroupType getType() {
+        return type;
+    }
+
+    public void setType(GroupType type) {
+        this.type = type;
+    }
+
     public Map<String, String> getGroupCondition() {
         return groupCondition;
     }
 
-    /**
-     * 添加分组条件项
-     */
-    public void putGroupCondition(String key,String condition){
-        this.groupCondition.put(key,condition);
+    public void putGroupCondition(String key, String condition) {
+        this.groupCondition.put(key, condition);
     }
+
 }
