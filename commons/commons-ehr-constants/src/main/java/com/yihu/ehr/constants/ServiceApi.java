754d80de281303ab5ac5b50b3d60cd39a543999b
Airhead <L4qiang@gmail.com>
2018-01-19 14:49:19 +0800

    测试数据包解析

--- commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/ServiceApi.java
+++ commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/ServiceApi.java
@@ -447,7 +447,7 @@ public class ServiceApi {
     public static class PackageAnalyzer {
         public static final String Scheduler = "packAnalyzer/scheduler";
         public static final String Queue = "packAnalyzer/queue";
-        public static final String Status = "packAnalyzer/status";
+        public static final String Status = "packAnalyzer/status/{id}";
     }
 
 
