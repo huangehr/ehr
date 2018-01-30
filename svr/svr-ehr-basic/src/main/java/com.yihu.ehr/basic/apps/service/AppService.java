56fe2ae6a1b8c2bac2dd313d6734342b6daffbf7
zdm <763558454@qq.com>
2018-01-30 20:36:39 +0800

    bug-4937 【基础信息管理】基础信息管理-应用信息：应用处于被审核状态或者被否决状态，不应可以对外展示。-- by zdm

--- svr/svr-ehr-basic/src/main/java/com.yihu.ehr/basic/apps/service/AppService.java
+++ svr/svr-ehr-basic/src/main/java/com.yihu.ehr/basic/apps/service/AppService.java
@@ -131,7 +131,7 @@ public class AppService extends BaseJpaService<App, AppRepository> {
                         "   b.source_type as sourceType, b.release_flag as releaseFlag, b.manage_type AS manageType" +
                         "   FROM apps b " +
                         "LEFT JOIN user_app m on m.app_id=b.id " +
-                        "WHERE b.catalog= :catalog AND m.user_id=:userId AND m.show_flag='1'";
+                        "WHERE b.catalog= :catalog AND m.user_id=:userId AND m.show_flag='1' AND b.status='Approved'";
         if (!StringUtils.isEmpty(manageType)) {
             sql += "AND b.manage_type = :manageType";
         }
