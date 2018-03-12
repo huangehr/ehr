f0232f02e460940076731680efa6d431134452a0
zhengwei <663785556@qq.com>
2018-03-12 17:40:53 +0800

    上传质控包接口修改

--- svr/svr-ehr-basic/src/main/java/com.yihu.ehr/basic/report/controller/QcDailyReportResolveController.java
+++ svr/svr-ehr-basic/src/main/java/com.yihu.ehr/basic/report/controller/QcDailyReportResolveController.java
@@ -1,17 +1,16 @@
 package com.yihu.ehr.basic.report.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.yihu.ehr.basic.report.service.JsonReportService;
 import com.yihu.ehr.basic.report.service.QcDailyReportResolveService;
 import com.yihu.ehr.basic.security.service.UserSecurityService;
 import com.yihu.ehr.basic.statistics.feign.DailyReportClient;
 import com.yihu.ehr.constants.ApiVersion;
-import com.yihu.ehr.constants.ErrorCode;
 import com.yihu.ehr.controller.EnvelopRestEndPoint;
 import com.yihu.ehr.entity.report.JsonReport;
 import com.yihu.ehr.entity.security.UserSecurity;
 import com.yihu.ehr.exception.ApiException;
+import com.yihu.ehr.util.compress.Zipper;
 import com.yihu.ehr.util.encrypt.RSA;
 import com.yihu.ehr.util.rest.Envelop;
 import io.swagger.annotations.Api;
@@ -26,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -60,7 +58,7 @@ public class QcDailyReportResolveController extends EnvelopRestEndPoint {
     @ApiOperation(value = "接收质控包")
     Envelop receiveReportFile(
             @ApiParam(name = "reportFile", value = "质控包", allowMultiple = true)
-            @RequestParam(value = "reportFile") MultipartFile reportFile,
+            @RequestParam(value = "reportFile") File reportFile,
             @ApiParam(name = "org_code", value = "机构代码")
             @RequestParam(value = "org_code") String orgCode,
             @ApiParam(name = "encrypt_pwd", value = "解压密码,二次加密")
@@ -68,23 +66,36 @@ public class QcDailyReportResolveController extends EnvelopRestEndPoint {
             @ApiParam(name = "md5", value = "档案包MD5")
             @RequestParam(value = "md5", required = false) String md5,
             @ApiParam(name = "type", value = "文件包类型 1 质控包 2 日报包")
-            @RequestParam(value = "type", defaultValue = "1", required = true) int type) throws Exception {
+            @RequestParam(value = "type", defaultValue = "1", required = true) int type
+    ) throws Exception {
         Envelop envelop = new Envelop();
         String password = null;
-        UserSecurity key = userSecurityService.getKeyByOrgCode(orgCode);
-        if (key == null || key.getPrivateKey() == null) {
-            throw new ApiException(HttpStatus.FORBIDDEN, ErrorCode.REQUEST_FORBIDDEN, "Invalid private key, maybe you miss the organization code?");
-        }
-        password = RSA.decrypt(encryptPwd, RSA.genPrivateKey(key.getPrivateKey()));
-        InputStream in =  reportFile.getInputStream();
-        InputStream stream =  reportFile.getInputStream();
-        JsonReport jsonReport = reportService.receive(in, password, encryptPwd, md5, orgCode, type);
-        JsonNode jsonNode = objectMapper.readTree(stream);
-        saveQcPackage(jsonNode,jsonReport);
-        if (jsonReport != null) {
-            envelop.setSuccessFlg(true);
-        } else {
-            envelop.setSuccessFlg(false);
+        try {
+            UserSecurity key = userSecurityService.getKeyByOrgCode(orgCode);
+            if (key == null || key.getPrivateKey() == null) {
+                throw new ApiException(HttpStatus.FORBIDDEN, "Invalid private key, maybe you miss the organization code?");
+            }
+            System.out.println(RSA.encrypt("p5Tm4unF",RSA.genPublicKey(key.getPublicKey())));
+            password = RSA.decrypt(encryptPwd, RSA.genPrivateKey(key.getPrivateKey()));
+            InputStream in =  new FileInputStream(reportFile);
+            JsonReport jsonReport = reportService.receive(in, password, encryptPwd, md5, orgCode, type);
+            String fileName = TempPath+System.currentTimeMillis()+".zip";
+            File newFile = new File(fileName);
+            reportFile.renameTo(newFile);
+            Zipper zipper = new Zipper();
+            zipper.unzipFile(newFile, TempPath, password);
+            File file = new File(TempPath+"events.json");
+            JsonNode jsonNode = objectMapper.readTree(file);
+            newFile.delete();
+            file.delete();
+            saveQcPackage(jsonNode,jsonReport);
+            if (jsonReport != null) {
+                envelop.setSuccessFlg(true);
+            } else {
+                envelop.setSuccessFlg(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return envelop;
     }
