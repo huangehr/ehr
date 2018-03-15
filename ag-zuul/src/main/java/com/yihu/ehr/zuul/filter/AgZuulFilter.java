25b1a7dc3d6e46983b3802cacc7be33b5ecb0083
yuhuiyang <yhy23456@163.com>
2018-03-15 09:00:15 +0800

    预约挂号H5接入：提供二次握手的URL给总部进行二次验证。

--- ag-zuul/src/main/java/com/yihu/ehr/zuul/filter/AgZuulFilter.java
+++ ag-zuul/src/main/java/com/yihu/ehr/zuul/filter/AgZuulFilter.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.stereotype.Component;
@@ -57,7 +56,7 @@ public class AgZuulFilter extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
         String url = request.getRequestURI();
-        if (url.startsWith("/authentication/") || url.equals("/basic/api/v1.0/users/h5/handshake")) {
+        if (url.startsWith("/authentication/") || url.contains("/users/h5/handshake")) {
             return null;
         }
         String accessToken = this.extractToken(request);
