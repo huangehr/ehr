0599433211864d473388e7bf10bdd8afd906300a
zdm <763558454@qq.com>
2018-03-24 09:10:52 +0800

    sxy-serviceApi

--- ag-ehr-cloud/src/main/java/com/yihu/ehr/api/healthprofile/controller/HealthProfileEndPoint.java
+++ ag-ehr-cloud/src/main/java/com/yihu/ehr/api/healthprofile/controller/HealthProfileEndPoint.java
@@ -59,7 +59,7 @@ public class HealthProfileEndPoint extends BaseController {
 
 
         @ApiOperation("患者患病史JSON")
-        @RequestMapping(value = ServiceApi.Profiles.ProfileHistory, method = RequestMethod.GET)
+        @RequestMapping(value = ServiceApi.Profiles.PastHistory, method = RequestMethod.GET)
         public String ProfileHistory(
                 @ApiParam(name = "demographic_id", value = "身份证号",defaultValue="360101200006011131")
                 @RequestParam(value = "demographic_id", required = true) String demographic_id) throws Exception{
