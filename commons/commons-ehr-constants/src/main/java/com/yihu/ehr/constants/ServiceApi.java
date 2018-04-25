bb6085af5f02c459d3bd0ca4e52048d2b1d20bd7
zdm <763558454@qq.com>
2018-04-25 10:41:33 +0800

    获取用户信息

--- commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/ServiceApi.java
+++ commons/commons-ehr-constants/src/main/java/com/yihu/ehr/constants/ServiceApi.java
@@ -779,7 +779,7 @@ public class ServiceApi {
         public static final String UsersByTel = "/users/tel";
         public static final String H5Handshake = "/users/h5/handshake";
         public static final String UsersOfAppPhoneExistence = "/usersOfApp/user/onePhone/existence";
-        public static final String GetUserOfUsersOfApp = "/usersOfApp/users/{user_name}";
+        public static final String GetUserOfUsersOfApp = "/usersOfApp/users/getUserOfUsersOfApp";
 
 
         public static final String Save = "/users/save";
