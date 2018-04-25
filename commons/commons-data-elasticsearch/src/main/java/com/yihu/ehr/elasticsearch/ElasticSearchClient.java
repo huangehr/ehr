e18e13c97765f223133c9f2c8a1113ae33be8af0
Progr1mmer <653800209@qq.com>
2018-04-25 08:16:15 +0800

    入库任务触发调整

--- commons/commons-data-elasticsearch/src/main/java/com/yihu/ehr/elasticsearch/ElasticSearchClient.java
+++ commons/commons-data-elasticsearch/src/main/java/com/yihu/ehr/elasticsearch/ElasticSearchClient.java
@@ -144,7 +144,7 @@ public class ElasticSearchClient {
     public void voidUpdate (String index, String type, String id, Map<String, Object> source) throws DocumentMissingException {
         TransportClient transportClient = elasticSearchPool.getClient();
         try {
-            transportClient.prepareUpdate(index, type, id).setDoc(source).get();
+            transportClient.prepareUpdate(index, type, id).setDoc(source).setRetryOnConflict(5).get();
         } finally {
             elasticSearchPool.releaseClient(transportClient);
         }
@@ -284,7 +284,7 @@ public class ElasticSearchClient {
         } catch (Exception e) {
            e.printStackTrace();
            return new ArrayList<>();
-        }finally {
+        } finally {
             if (resultSet != null) {
                 resultSet.close();
             }
