c6887524a3b11860feecd6262e11b45eb834293c
Progr1mmer <653800209@qq.com>
2018-07-02 10:22:44 +0800

    分组

--- svr/svr-pack-resolve/src/main/java/com/yihu/ehr/resolve/LinkPackageResolver.java
+++ svr/svr-pack-resolve/src/main/java/com/yihu/ehr/resolve/LinkPackageResolver.java
@@ -173,7 +173,7 @@ public class LinkPackageResolver extends PackageResolver {
                     NameValuePair[] fileMetaData = new NameValuePair[1];
                     fileMetaData[0] = new NameValuePair("description", "File from link profile package.");
                     //影像文件存在group2
-                    ObjectNode msg = fastDFSUtil.uploadBySocket(groupName,inputStream, fileExtension, (int)fileSize,fileMetaData);
+                    ObjectNode msg = fastDFSUtil.uploadBySocket(groupName, inputStream, fileExtension, (int)fileSize, fileMetaData);
                     LinkFile linkFile = new LinkFile();
                     linkFile.setFileExtension(fileExtension);
                     linkFile.setOriginName(fileName);
@@ -186,7 +186,7 @@ public class LinkPackageResolver extends PackageResolver {
                     ftpUtils.deleteFile(path, fileName);
                 }
             } finally {
-                if(ftpUtils !=null){
+                if (ftpUtils != null){
                     ftpUtils.closeConnect();
                 }
             }
