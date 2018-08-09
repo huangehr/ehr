640c807eb4e4863204e2002431b734f4b1619276
chenyongxing <395422431@qq.com>
2018-08-10 00:10:23 +0800

    影像文件多次解析

--- svr/svr-pack-mgr/src/main/java/com/yihu/ehr/pack/task/PackAnalyzeTask.java
+++ svr/svr-pack-mgr/src/main/java/com/yihu/ehr/pack/task/PackAnalyzeTask.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihu.ehr.elasticsearch.ElasticSearchUtil;
 import com.yihu.ehr.model.packs.EsSimplePackage;
 import com.yihu.ehr.profile.queue.RedisCollection;
-import com.yihu.ehr.util.datetime.DateUtil;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,8 +15,6 @@ import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
-
-import static com.yihu.ehr.util.datetime.DateUtil.DEFAULT_YMDHMSDATE_FORMAT;
 
 /**
  * 档案包质控容错处理任务
@@ -44,13 +41,11 @@ public class PackAnalyzeTask {
     public void delayPushTask() throws Exception {
         List<String> esSimplePackageList = new ArrayList<>(200);
         //当质控队列为空，将状态为待质控的档案包加入质控队列
-        if (redisTemplate.opsForSet().size(RedisCollection.AnalyzeQueueVice) <= 0 ) {
-            Page<Map<String, Object>> result = elasticSearchUtil.page(INDEX, TYPE, "analyze_status=0", "+receive_date", 1, 1000);
+        if (redisTemplate.opsForSet().size(RedisCollection.AnalyzeQueueVice) <= 0) {
+            //影像档案,如果是待质控,不添加入次质控队列, 防止主消息队列刚消费完,还未进行更改状态,就添加到 次质控队列,进而添加到次解析队列,进行二次解析(此时,很可能主解析队列已经将该影像档案入库,将ftp文件干掉了!)
+            Page<Map<String, Object>> result = elasticSearchUtil.page(INDEX, TYPE, "analyze_status=0;pack_type<>3", "+receive_date", 1, 1000);
             for (Map<String, Object> pack : result) {
-                pack.put("analyze_status",1);
-                pack.put("message","加入消息队列,等待质控");
-                Map<String, Object> update = elasticSearchUtil.update(INDEX, TYPE, pack.get("_id") + "", pack);
-                String packStr = objectMapper.writeValueAsString(update);
+                String packStr = objectMapper.writeValueAsString(pack);
                 EsSimplePackage esSimplePackage = objectMapper.readValue(packStr, EsSimplePackage.class);
                 redisTemplate.opsForSet().add(RedisCollection.AnalyzeQueueVice, objectMapper.writeValueAsString(esSimplePackage));
             }
@@ -61,8 +56,7 @@ public class PackAnalyzeTask {
         for (Map<String, Object> pack : result) {
             Map<String, Object> updateSource = new HashMap<>();
             updateSource.put("_id", pack.get("_id"));
-            updateSource.put("analyze_status", 1);
-            updateSource.put("message","重新加入消息队列,等待质控");
+            updateSource.put("analyze_status", 0);
             updateSourceList.add(updateSource);
             String packStr = objectMapper.writeValueAsString(pack);
             EsSimplePackage esSimplePackage = objectMapper.readValue(packStr, EsSimplePackage.class);
@@ -75,9 +69,7 @@ public class PackAnalyzeTask {
         for (Map<String, Object> pack : result) {
             Map<String, Object> updateSource = new HashMap<>();
             updateSource.put("_id", pack.get("_id"));
-            updateSource.put("analyze_date",DateUtil.toStringLong(new Date()));
-            updateSource.put("analyze_status", 1);
-            updateSource.put("message","重新加入消息队列,等待质控");
+            updateSource.put("analyze_status", 0);
             updateSourceList.add(updateSource);
             String packStr = objectMapper.writeValueAsString(pack);
             EsSimplePackage esSimplePackage = objectMapper.readValue(packStr, EsSimplePackage.class);
--- svr/svr-pack-mgr/src/main/java/com/yihu/ehr/pack/task/PackResolveTask.java
+++ svr/svr-pack-mgr/src/main/java/com/yihu/ehr/pack/task/PackResolveTask.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yihu.ehr.elasticsearch.ElasticSearchUtil;
 import com.yihu.ehr.model.packs.EsSimplePackage;
 import com.yihu.ehr.profile.queue.RedisCollection;
-import com.yihu.ehr.util.datetime.DateUtil;
 import org.apache.commons.lang.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -43,12 +42,10 @@ public class PackResolveTask {
         List<String> esSimplePackageList = new ArrayList<>(200);
         //当解析队列为空，将数据库中状态为缓存状态的档案包加入解析队列
         if (redisTemplate.opsForSet().size(RedisCollection.ResolveQueueVice) <= 0) {
-            Page<Map<String, Object>> result = elasticSearchUtil.page(INDEX, TYPE, "analyze_status=3;archive_status=0", "+receive_date", 1, 1000);
+            //影像档案,如果是待解析,不添加入次解析队列, 防止主消息队列刚消费完,还未进行更改状态,就添加到 次解析队列,进行二次解析(此时,主解析队列已经入库,将ftp文件干掉了)
+            Page<Map<String, Object>> result = elasticSearchUtil.page(INDEX, TYPE, "analyze_status=3;archive_status=0;pack_type<>3", "+receive_date", 1, 1000);
             for (Map<String, Object> pack : result) {
-                pack.put("archive_status",1);
-                pack.put("message","加入消息队列,等待入库");
-                Map<String, Object> update = elasticSearchUtil.update(INDEX, TYPE, pack.get("_id") + "", pack);
-                String packStr = objectMapper.writeValueAsString(update);
+                String packStr = objectMapper.writeValueAsString(pack);
                 EsSimplePackage esSimplePackage = objectMapper.readValue(packStr, EsSimplePackage.class);
                 redisTemplate.opsForSet().add(RedisCollection.ResolveQueueVice, objectMapper.writeValueAsString(esSimplePackage));
             }
@@ -59,8 +56,7 @@ public class PackResolveTask {
         for (Map<String, Object> pack : result) {
             Map<String, Object> updateSource = new HashMap<>();
             updateSource.put("_id", pack.get("_id"));
-            updateSource.put("archive_status", 1);
-            updateSource.put("message","重新加入消息队列,等待入库");
+            updateSource.put("archive_status", 0);
             updateSourceList.add(updateSource);
             String packStr = objectMapper.writeValueAsString(pack);
             EsSimplePackage esSimplePackage = objectMapper.readValue(packStr, EsSimplePackage.class);
@@ -73,9 +69,7 @@ public class PackResolveTask {
         for (Map<String, Object> pack : result) {
             Map<String, Object> updateSource = new HashMap<>();
             updateSource.put("_id", pack.get("_id"));
-            updateSource.put("archive_status", 1);
-            updateSource.put("message","重新加入消息队列,等待入库");
-            updateSource.put("parse_date",DateUtil.toStringLong(new Date()));
+            updateSource.put("archive_status", 0);
             updateSourceList.add(updateSource);
             String packStr = objectMapper.writeValueAsString(pack);
             EsSimplePackage esSimplePackage = objectMapper.readValue(packStr, EsSimplePackage.class);
